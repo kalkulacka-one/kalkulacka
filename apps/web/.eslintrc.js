b024f1453d470165f3c4e3a418cdbbbc9981c6cf
Kryštof Korb <krystof@korb.cz>
2024-05-28 22:36:52 +0200

    Set up Tailwind CSS in web app

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -6,4 +6,7 @@ module.exports = {
   parserOptions: {
     project: true,
   },
+  env: {
+    node: true,
+  },
 };
---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,9 @@
+import globalConfig from "@repo/tailwind-config";
+import type { Config } from "tailwindcss";
+
+const config: Pick<Config, "content" | "presets"> = {
+  content: ["./app/**/*.tsx"],
+  presets: [globalConfig],
+};
+
+export default config;
