7228c08b6a021398ed4427ec891643f12fb880c5
Kryštof Korb <krystof@korb.cz>
2024-05-29 15:55:56 +0200

    Set up fonts for web app

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,9 +1,20 @@
 import globalConfig from "@repo/tailwind-config";
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const fonts = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Radio Canada"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
 
 const config: Pick<Config, "content" | "presets"> = {
   content: ["./app/**/*.tsx"],
-  presets: [globalConfig],
+  presets: [globalConfig, fonts],
 };
 
 export default config;
