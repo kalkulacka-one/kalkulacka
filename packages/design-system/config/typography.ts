76b4eab2c4fdc4d471f3a916bfd35aeedc3ff359
Kryštof Korb <krystof@korb.cz>
2024-07-25 11:27:37 +0200

    Fix formatting (#39)

--- packages/design-system/config/typography.ts
+++ packages/design-system/config/typography.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const typography: Pick<Config, "theme"> = {
   theme: {
