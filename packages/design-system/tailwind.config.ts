fda60aed6342084e8e5adc2760d0c0fdfde1cd9a
Kryštof Korb <krystof@korb.cz>
2024-07-25 09:26:56 +0200

    Fix formatting

--- packages/design-system/tailwind.config.ts
+++ packages/design-system/tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 import globalConfig from "@repo/tailwind-config";
 
 import color from "./config/color.ts";
-import typography from "./config/typography.ts"
+import typography from "./config/typography.ts";
 
 const config: Pick<Config, "content" | "prefix" | "presets"> = {
   content: ["./src/**/*.tsx"],
