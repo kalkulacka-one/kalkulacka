ef75607c0ab83d05068feda22ca1880a2c75aab5
Kryštof Korb <krystof@korb.cz>
2024-07-25 11:57:48 +0200

    Add example color tokens

--- packages/design-system/config/color.ts
+++ packages/design-system/config/color.ts
@@ -1,11 +1,19 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const color: Pick<Config, "theme"> = {
   theme: {
     extend: {
       colors: {
-        red: {
-          100: "#BB0000",
+        background: {
+          DEFAULT: `var(--k1-color-background-light, ${colors.white})`,
+          light: `var(--k1-color-background-light, ${colors.white})`,
+          dark: `var(--k1-color-background-dark, ${colors.gray[900]})`,
+        },
+        foreground: {
+          DEFAULT: `var(--k1-color-foreground-light, ${colors.gray[900]})`,
+          light: `var(--k1-color-foreground-light, ${colors.gray[900]})`,
+          dark: `var(--k1-color-foreground-dark, ${colors.white})`,
         },
       },
     },
