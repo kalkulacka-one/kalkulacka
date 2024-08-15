2471cc441aad71a95a8ab4d9babaeeb8afe20014
nicas <nica.sergiu@seznam.cz>
2024-08-15 10:07:40 +0100

    Created a UI component for Storybook - Badge

---
+++ apps/design-system/stories/badge.stories.tsx
@@ -0,0 +1,42 @@
+import { Badge }  from "../../../packages/design-system/src/badge";
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  
+  title: 'Components/Badge',
+  component: Badge,
+  argTypes: {
+    color: { control: 'color' },
+    icon: { control: 'boolean' },
+    variant: {
+      control: { type: 'select', options: ['neutral', 'primary', 'secondary'] },
+    },
+  },
+  tags: ['autodocs'],
+} as Meta;
+
+type Story = StoryObj<typeof Badge>;	
+
+export const Neutral : Story = {
+  args: {
+    icon: false,
+    variant: 'neutral',
+    children: 'Label',
+  },
+};
+
+export const Primary : Story = {
+  args : {
+    icon: true,
+    variant: 'primary',
+    children: 'Label',
+  }
+};
+
+export const Secondary : Story = {
+  args : {
+    icon: false,
+    variant: 'secondary',
+    children: 'Label',
+  }
+};
