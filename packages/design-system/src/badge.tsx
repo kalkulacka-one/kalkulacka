7d2b2f36a003dce67fb2e1f8657311991b6ae2a9
nicas <nica.sergiu@seznam.cz>
2024-08-20 12:24:47 +0100

    Fix for passing build & lint test

--- packages/design-system/src/badge.tsx
+++ packages/design-system/src/badge.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import BadgeStarIcon from "./Badge/BadgeStarIcon"
+import React from "react";
+import BadgeStarIcon from "./Badge/BadgeStarIcon";
 
 export interface BadgeProps {
   icon?: boolean;
-  variant?: 'neutral' | 'secondary' | 'primary';
+  variant?: "neutral" | "secondary" | "primary";
   children: React.ReactNode;
 }
 
-const Badge: React.FC<BadgeProps> = ({ icon = false, variant = 'primary', children }) => {
-  const showIcon = icon || variant === 'primary';
-  const neutralVariant = variant === 'primary' || variant === 'neutral'
-  
+const Badge: React.FC<BadgeProps> = ({
+  icon = false,
+  variant = "primary",
+  children,
+}) => {
+  const showIcon = icon || variant === "primary";
+  const neutralVariant = variant === "primary" || variant === "neutral";
+
   return (
-    <div className={
-      `k1-flex k1-flex-row k1-justify-center k1-items-center rounded-[0.375rem] px-[0.5rem] py-[0.25rem] k1-gap-1 m-0 font-bold uppercase relative ${showIcon ? 'k1-w-[4.5rem]' : 'k1-w-[3.125rem]'} ${showIcon ? 'k1-h-[1.625rem]' : 'k1-h-[1.5rem]'}`
-    }>  
-      {showIcon && <div className="k1-inline-block k1-items-center"><BadgeStarIcon/></div>}
-      <div className = {
-        `flex text-[0.625rem] font-bold leading-[1.125rem] k1-border-hidden k1-outline-none w-[2.125rem] h-[1.125rem] k1-items-center k1-justify-center k1-inline-block 
-        ${neutralVariant ? 'k1-text-[#565252]' : 'k1-text-[#821414]'} 
-        ${neutralVariant ? 'bg-[#FFF4F5]' : 'bg-[#F8F7F7]'}`
-      }
-        >
+    <div
+      className={`k1-flex k1-flex-row k1-justify-center k1-items-center rounded-[0.375rem] px-[0.5rem] py-[0.25rem] k1-gap-1 m-0 font-bold uppercase relative ${showIcon ? "k1-w-[4.5rem]" : "k1-w-[3.125rem]"} ${showIcon ? "k1-h-[1.625rem]" : "k1-h-[1.5rem]"}`}
+    >
+      {showIcon && (
+        <div className="k1-inline-block k1-items-center">
+          <BadgeStarIcon />
+        </div>
+      )}
+      <div
+        className={`flex text-[0.625rem] font-bold leading-[1.125rem] k1-border-hidden k1-outline-none w-[2.125rem] h-[1.125rem] k1-items-center k1-justify-center k1-inline-block 
+        ${neutralVariant ? "k1-text-[#565252]" : "k1-text-[#821414]"} 
+        ${neutralVariant ? "bg-[#FFF4F5]" : "bg-[#F8F7F7]"}`}
+      >
         {children}
       </div>
     </div>
@@ -29,6 +36,3 @@ const Badge: React.FC<BadgeProps> = ({ icon = false, variant = 'primary', childr
 };
 
 export { Badge };
-
-
-
