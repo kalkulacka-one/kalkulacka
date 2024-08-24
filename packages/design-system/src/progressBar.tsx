d83df1c6e02c4ff347bbc8c10c0a4b44cf5ef400
gloomo <107949187+gluciox@users.noreply.github.com>
2024-08-24 16:26:44 +0200

    progress indicator, twconfig, bare variables

---
+++ packages/design-system/src/progressBar.tsx
@@ -0,0 +1,41 @@
+import { cva, VariantProps } from "class-variance-authority";
+
+type Props = {
+  progress: number;
+} & VariantProps<typeof progressBarVariants>;
+
+const progressBarVariants = cva("k1-h-full k1-w-full", {
+  variants: {
+    color: {
+      blue: "k1-bg-primary-fg",
+      red: "k1-bg-red-500",
+      orange: "k1-bg-orange-500",
+      yellow: "k1-bg-yellow-500",
+    },
+  },
+  defaultVariants: {
+    color: "blue",
+  },
+});
+
+const ProgressBar = ({ progress, color }: Props): JSX.Element => {
+  const width = progress > 100 ? 100 : progress < 0 ? 0 : progress;
+
+  return (
+    <div
+      className="k1-bg-neutral-bg k1-h-0.5 lg:k1-h-1.5 k1-w-full k1-overflow-hidden k1-rounded-sm"
+      role="progressbar"
+      aria-valuenow={width}
+      aria-valuemin={0}
+      aria-valuemax={100}
+      aria-label={`${width}%`}
+    >
+      <div
+        className={progressBarVariants({ color })}
+        style={{ transform: `translateX(-${100 - (width || 0)}%)` }}
+      ></div>
+    </div>
+  );
+};
+
+export { ProgressBar, progressBarVariants };
