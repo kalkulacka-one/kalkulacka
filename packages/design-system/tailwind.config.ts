caeac60dcded3c6ee6947b71a3138d6e963dcb88
martinwenisch <martin@muhu.digital>
2024-08-24 17:56:28 +0200

    Theme css and dynamic import in Storybook

--- packages/design-system/tailwind.config.ts
+++ packages/design-system/tailwind.config.ts
@@ -2,13 +2,9 @@ import type { Config } from "tailwindcss";
 
 import globalConfig from "@repo/tailwind-config";
 
-import color from "./config/color.ts";
-import typography from "./config/typography.ts";
-
 const config: Pick<Config, "content" | "prefix" | "presets"> = {
   content: ["./src/**/*.tsx"],
-  prefix: "k1-",
-  presets: [globalConfig, color, typography],
+  presets: [globalConfig],
 };
 
 export default config;
--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,13 +1,25 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 // Each package is responsible for its own content
 const config: Omit<Config, "content"> = {
   theme: {
     extend: {
       colors: {
-        red: {
-          100: "#FF0000",
+        primary: {
+          100: "var(--primary-100)",
+          50: "var(--primary-50)",
         },
+        secondary: {
+          50: "var(--secondary-50)",
+        },
+        neutral: {
+          100: "var(--neutral-100)",
+          50: "var(--neutral-50)",
+        },
+      },
+      fontFamily: {
+        sans: ['"Radio Canada"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
