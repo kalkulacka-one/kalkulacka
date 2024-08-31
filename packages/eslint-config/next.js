ca0743fb46bc61ef8de67e826b0699418888b723
martinwenisch <martin@muhu.digital>
2024-08-31 14:53:53 +0200

    Merge prettier into lint command

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -9,6 +9,7 @@ module.exports = {
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
     "eslint-config-turbo",
+    "plugin:prettier/recommended",
   ],
   globals: {
     React: true,
