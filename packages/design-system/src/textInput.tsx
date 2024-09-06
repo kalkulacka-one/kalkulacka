690d3a83bb5784a9f34505a2094e74cf2827feb9
ser888gio <nica.sergiu@seznam.cz>
2024-09-06 12:17:04 +0200

    Fixed the Tailwind css + fixed the cva and tailwind problem

--- packages/design-system/src/textInput.tsx
+++ packages/design-system/src/textInput.tsx
@@ -4,9 +4,9 @@ import { Field } from "@headlessui/react";
 import ErrorIcon from "./Icons/ErrorIcon";
 import { useState } from "react";
 import { cva } from "class-variance-authority";
-import { InputWithIcon } from "./input";
+import { InputWithIcon, InputProps } from "./input";
 
-export interface textInputProps {
+type textInputProps = Partial<InputProps> & {
   //změnit na typ, musí dědit od HTMLInputProps (podle komponenty Input)
   disabled?: boolean | undefined; //if true, the input is disabled
   groupName?: string | undefined;
@@ -15,20 +15,20 @@ export interface textInputProps {
   modelValue?: string | undefined; //represents the value of the input. To think, if it is necessary
   placeholder?: string | undefined; //displays value before user inputs something
   error?: string | null; //represents the error message
-}
+};
 
 const textInputStyles = cva(
   [
-    "k1-h-[3.5rem] k1-w-[18.5rem] k1-p-4 k1-flex k1-flex-row k1-flex-no-wrap k1-justify-start k1-items-center k1-relative k1-border-solid k1-border-2 k1-rounded-l-3xl k1-rounded-tr-2xl",
+    "k1-inline-flex k1-p-4 k1-flex-row k1-justify-start k1-items-center k1-relative k1-border-solid k1-border-2 k1-rounded-tl k1-rounded-tr k1-rounded-bl k1-rounded-br-none",
   ],
   {
     variants: {
       variant: {
-        default: ["k1-border-neutral-border", "k1-text-[#1D1C1C]"],
-        hover: ["k1-border-neutral-border-hover", "k1-text-[#ADA6A6]"],
-        readonly: ["k1-border-neutral-fg-disabled", "k1-text-[#565252]"],
-        active: ["k1-border-neutral-border-active", "k1-text-[#1D1C1C]"],
-        error: ["k1-border-secondary-border-strong", "k1-text-[#D04646]"],
+        default: ["k1-border-neutral-disabled", "k1-text-neutral"],
+        hover: ["k1-border-neutral-hover", "k1-text-neutral"],
+        readonly: ["k1-border-neutral-disabled", "k1-text-neutral-muted"],
+        active: ["k1-border-neutral-active", "k1-text-neutral-strong"],
+        error: ["k1-border-secondary-strong", "k1-text-secondary-hover"],
       },
     },
   },
@@ -62,7 +62,7 @@ const TextInput = ({
 
   return (
     <div className={textInputStyles({ variant: determineState() })}>
-      <Field className="input-field">
+      <Field className="input-field ">
         {label && <Label>{label}</Label>}
         <div className="input-container k1-flex">
           {showIcon && <HomeIconComponent className="k1-mr-2" />}
@@ -84,7 +84,7 @@ const TextInput = ({
           />
         </div>
         {error && (
-          <div className="k1-pl-1 k1-pr-1 k1-flex-row k1-justify-between k1-items-center k1-absolute k1-right-[0.75rem] k1-bottom-[-0.75rem] k1-bg-neutral-fg-hover k1-flex">
+          <div className="k1-pl-1 k1-pr-1 k1-flex-row k1-justify-between k1-items-center k1-absolute k1-right-[0.75rem] k1-bottom-[-0.75rem] k1-bg-neutral-fg-hover k1-flex k1-bg-neutral-container">
             <div>{error}</div>
             <ErrorIcon className="k1-ml-1" />
           </div>
