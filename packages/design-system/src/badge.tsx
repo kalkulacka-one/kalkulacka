951609cd89b5999a7b65a3aea56335a4b2ff6fd6
ser888gio <nica.sergiu@seznam.cz>
2024-09-06 12:19:09 +0200

    Fixed the tailwind

--- packages/design-system/src/badge.tsx
+++ packages/design-system/src/badge.tsx
@@ -10,28 +10,17 @@ export interface BadgeProps {
 }
 
 
-const badge = cva(["k1-flex k1-flex-row k1-justify-center k1-items-center rounded-[0.375rem] px-[0.5rem] py-[0.25rem] k1-gap-1 m-0 font-bold uppercase relative"], {
-  variants: {
+const badge = cva(
+  [
+    "k1-flex k1-flex-row k1-rounded-[0.375rem] k1-px-[0.5rem] k1-py-[0.25rem] k1-gap-1 k1-m-0 k1-font-bold k1-uppercase k1-relative k1-inline-block k1-max-w-fit",
+  ],
+  {
+    variants: {
       color: {
-          neutral: [
-              "k1-text-[#565252]",
-              "bg-[#FFF4F5]"
-          ],
-          secondary: [
-              "k1-text-[#821414]",
-               "bg-[#F8F7F7]"
-          ]     
-          },
-      size: {
-          with_icon: [
-              "k1-w-[4.5rem]",
-              "k1-h-[1.625rem]"
-          ],
-          no_icon: [
-              "k1-w-[3.125rem]", 
-              "k1-h-[1.5rem]"
-          ]
-      }
+        neutral: ["k1-text-neutral", "k1-bg-neutral"],
+        secondary: ["k1-text-secondary-hover", "k1-bg-secondary"],
+      },
+    },
   },
 })
 
@@ -45,15 +34,18 @@ const Badge: React.FC<BadgeProps> = ({
   const neutralVariant = color === "neutral";
 
   return (
-    <div className={badge({ color: neutralVariant ? 'neutral' : 'secondary', size: showIcon ? 'with_icon' : 'no_icon' })}
-    >    
-        {showIcon && (
+    <div
+      className={badge({
+        color: neutralVariant ? "neutral" : "secondary",
+      })}
+    >
+      {showIcon && (
         <div className="k1-inline-block k1-items-center">
           <BadgeStarIcon />
         </div>
       )}
       <div
-        className={`flex text-[0.625rem] font-bold leading-[1.125rem] k1-border-hidden k1-outline-none w-[2.125rem] h-[1.125rem] k1-items-center k1-justify-center k1-inline-block`}   
+        className={`k1-text-[0.625rem] k1-font-bold k1-leading-4 k1-border-hidden k1-outline-none w-[2.125rem] h-[1.125rem] k1-items-center k1-justify-center k1-inline-block k1-tracking-[0.04] k1-align-bottom`}
       >
         {children}
       </div>
