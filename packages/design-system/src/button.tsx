5b1eb74c41e08b1a19596cfd9f814646e8a1a4ae
mewdev <michal.wierzgon@gmail.com>
2024-09-11 11:41:58 +0200

    css optimized (with icon and without), button variants previews added, demo icons added for testing purposes

--- packages/design-system/src/button.tsx
+++ packages/design-system/src/button.tsx
@@ -4,33 +4,39 @@ import { cva, VariantProps } from "class-variance-authority";
 import { Slot } from "@radix-ui/react-slot";
 
 const buttonVariants = cva(
-  "k1-uppercase k1-font-bold k1-rounded-tl-2xl k1-rounded-bl-2xl k1-rounded-br-2xl",
+  "k1-inline-flex k1-items-center k1-justify-center hover:k1-cursor-pointer k1-text-nowrap disabled:k1-pointer-events-none k1-uppercase k1-font-bold",
   {
     variants: {
       variant: {
-        // defaul == filled ??
         default:
-          "k1-text-[#FFFFFF] k1-bg-[#0070F4] hover:k1-bg-[#0A3EAF] active:k1-bg-[#091233] disabled:k1-bg-[#DAD8D7]",
+          "k1-rounded-l k1-rounded-br k1-text-left k1-w-full k1-text-neutral-inverse k1-bg-primary-strong hover:k1-bg-primary-strong-hover active:k1-bg-primary-strong-active disabled:k1-bg-neutral-disaled",
         filled:
-          "k1-text-[#FFFFFF] k1-bg-[#0070F4] hover:k1-bg-[#0A3EAF] active:k1-bg-[#091233] disabled:k1-bg-[#DAD8D7]",
-        // backdrop colors use predefined by designer or opacity tw class?
+          "k1-rounded-l k1-rounded-br k1-w-full k1-text-neutral-inverse k1-bg-primary-strong hover:k1-bg-primary-strong-hover active:k1-bg-primary-strong-active disabled:k1-bg-neutral-disaled",
         outline:
-          "k1-text-[#565252] hover:k1-bg-[#DAD8D7] hover:k1-bg-opacity-20 active:k1-bg-[#DAD8D7] active:k1-bg-opacity-60 k1-border-2 disabled:k1-bg-[#DAD8D7] disabled:k1-border-[#DAD8D7] k1-border-[#565252]",
-        link: "k1-text-[#565252] k1-p-0 hover:k1-bg-[#ADA6A6] active:k1-bg-[#000000] disabled:k1-bg-[#DAD8D7]",
-        // fix borders shit, shadows
+          "k1-rounded-l k1-rounded-br k1-w-full k1-border-2 k1-text-neutral k1-border-neutral-strong hover:k1-text-neutral-strong hover:k1-bg-neutral-backdrop-hover active:k1-bg-neutral-backdrop-active active:k1-text-neutral-active disabled:k1-text-neutral-disabled disabled:k1-bg-neutral-disaled disabled:k1-border-neutral-disabled",
+        link: "k1-gap-2 k1-w-auto k1-text-neutral hover:k1-text-neutral-hover active:k1-text-neutral-active disabled:k1-text-neutral-disabled ",
         answerInFavour:
-          "k1-border-[#0070F4] k1-border-2 k1-py-4 k1-px-6 k1-text-[#0070F4] hover:k1-bg-[#0A3EAF] hover:k1-text-[#FFFFFF] hover:k1-border-none active:k1-bg-[#0070F4] active:k1-text-[#FFFFFF]",
+          "k1-rounded-l k1-rounded-br k1-w-full k1-border-2 k1-text-primary k1-border-primary-strong hover:k1-bg-primary-strong-hover hover:k1-text-neutral-inverse hover:k1-shadow-neutral-70 hover:k1-border-primary-strong-hover active:k1-bg-primary-strong active:k1-text-neutral-inverse active:k1-border-primary-strong",
         answerAgainst:
-          "k1-border-[#D04646] k1-border-2 k1-py-4 k1-px-6 k1-text-[#D04646] hover:k1-bg-[#821414] hover:k1-text-[#FFFFFF] hover:k1-border-none active:k1-bg-[#D04646] active:k1-text-[#FFFFFF]",
-
+          "k1-rounded-l k1-rounded-br k1-w-full k1-border-2 k1-text-secondary k1-border-secondary-strong hover:k1-bg-secondary-strong-hover hover:k1-text-neutral-inverse hover:k1-shadow-neutral-70 hover:k1-border-secondary-strong-hover active:k1-bg-secondary-strong active:k1-text-neutral-inverse active:k1-border-secondary-strong",
         answerNeutral:
-          "k1-border-[#565252] k1-border-2 k1-py-4 k1-px-6 k1-text-[#565252] hover:k1-bg-[#565252] hover:k1-text-[#FFFFFF] active:k1-bg-[#1D1C1C] active:k1-text-[#FFFFFF]",
+          "k1-rounded-l k1-rounded-br k1-w-full k1-border-2 k1-text-neutral k1-border-neutral-strong hover:k1-bg-neutral-strong-hover hover:k1-text-neutral-inverse hover:k1-shadow-neutral-70 hover:k1-border-neutral-strong-hover active:k1-bg-neutral-strong-active active:k1-text-neutral-inverse active:k1-border-neutral-active",
+        icon: "k1-p-2 k1-rounded-full k1-text-neutral hover:k1-bg-neutral-backdrop-hover active:k1-bg-neutral-backdrop-active",
       },
       size: {
-        default: "k1-p-4 k1-text-sm k1-tracking-wider k1-leading-6",
-        sm: "k1-px-4 k1-py-2 k1-text-xs k1-tracking-wider k1-leading-4",
-        icon: "k1-p-4",
+        default: "k1-h-14 k1-px-4 k1-text-sm k1-tracking-wider k1-leading-6",
+        sm: "k1-h-10 k1-px-4 k1-text-xs k1-tracking-wider k1-leading-4",
+        iconSm: "k1-h-8 k1-w-8",
+        iconDefault: "k1-h-10 k1-w-10",
+        iconLg: "k1-h-14 k1-w-14",
+        linkDefault: "k1-h-6",
+        linkSm: "k1-h-4",
       },
+    },
+
+    defaultVariants: {
+      variant: "filled",
+      size: "default",
     },
   }
 );
