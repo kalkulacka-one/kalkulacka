456a774e7820d97c8e7edd82f071c240b9cefa93
mewdev <michal.wierzgon@gmail.com>
2024-09-22 13:18:51 +0300

    toggle working (render props approach), needs greater flexibility (toggle with more than one element)

--- apps/design-system/stories/toggle.stories.tsx
+++ apps/design-system/stories/toggle.stories.tsx
@@ -9,6 +9,19 @@ const meta: Meta<typeof Toggle> = {
 
 type ToggleStory = StoryObj<typeof meta>;
 
-export const Default: ToggleStory = {};
+export const Default: ToggleStory = {
+  args: {
+    children: (checked) => (
+      <button
+        style={{
+          transition: "background-color ease-in-out 300ms",
+          backgroundColor: checked ? "#2563eb" : "#e5e7eb", // Adjust these hex codes to match your Tailwind colors
+        }}
+      >
+        Toggle button
+      </button>
+    ),
+  },
+};
 
 export default meta;
