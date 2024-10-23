bc3584bf339ce142b53dc328c582bacf52272eca
ser888gio <nica.sergiu@seznam.cz>
2024-10-23 09:05:37 +0200

    Changed the UI library for breadcrumbs from MUI Material to Shadcn Radix UI

--- packages/design-system/src/breadcrumbs.tsx
+++ packages/design-system/src/breadcrumbs.tsx
@@ -1,38 +1,123 @@
 import * as React from "react";
-import { Breadcrumbs as MuiBreadcrumbs } from "@mui/material";
-import cn from "classnames";
+import { Slot } from "@radix-ui/react-slot";
+import { twMerge } from "tailwind-merge";
+import { ChevronRightIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
 
-interface BreadcrumbItem {
-  label: string;
-  href: string;
-}
+//DOCS: https://ui.shadcn.com/docs/components/breadcrumb
 
-interface BreadcrumbsProps {
-  items: BreadcrumbItem[];
-  className?: string;
-}
+// Forward ref for Breadcrumb component, which is a <nav> element with aria-label "breadcrumb"
+const Breadcrumb = React.forwardRef<
+  HTMLElement,
+  React.ComponentPropsWithoutRef<"nav">
+>(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />);
 
-const Breadcrumbs = React.forwardRef<HTMLDivElement, BreadcrumbsProps>(
-  ({ items, className }, ref) => (
-    <MuiBreadcrumbs
+// Set display name for Breadcrumb component
+Breadcrumb.displayName = "Breadcrumb";
+
+// Forward ref for BreadcrumbList component, which is an <ol> element with specific class names
+const BreadcrumbList = React.forwardRef<
+  HTMLOListElement, // Forwarding ref to <ol> element
+  React.ComponentPropsWithoutRef<"ol"> // Props for <ol> element
+>(({ className, ...props }, ref) => (
+  <ol
+    ref={ref}
+    className={twMerge(
+      "flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5",
+      className
+    )}
+    {...props}
+  />
+));
+
+// Set display name for BreadcrumbList component
+BreadcrumbList.displayName = "BreadcrumbList";
+
+// Forward ref for BreadcrumbItem component, which is a <li> element
+const BreadcrumbItem = React.forwardRef<
+  HTMLLIElement,
+  React.ComponentPropsWithoutRef<"li">
+>(({ className, ...props }, ref) => (
+  <li
+    ref={ref}
+    className={twMerge("inline-flex items-center gap-1.5", className)}
+    {...props}
+  />
+));
+BreadcrumbItem.displayName = "BreadcrumbItem";
+
+// Forward ref for BreadcrumbItem component, which is a <anchor> element
+const BreadcrumbLink = React.forwardRef<
+  HTMLAnchorElement,
+  React.ComponentPropsWithoutRef<"a"> & {
+    asChild?: boolean;
+  }
+>(({ asChild, className, ...props }, ref) => {
+  const Comp = asChild ? Slot : "a";
+
+  return (
+    <Comp
       ref={ref}
-      separator="-"
-      aria-label="breadcrumb"
-      className={cn("", className)}
-    >
-      {items.map((item, index) =>
-        index === items.length - 1 ? (
-          <span key={item.href}>{item.label}</span>
-        ) : (
-          <a key={item.href} href={item.href}>
-            {item.label}
-          </a>
-        ),
-      )}
-    </MuiBreadcrumbs>
-  ),
+      className={twMerge("transition-colors hover:text-foreground", className)}
+      {...props}
+    />
+  );
+});
+BreadcrumbLink.displayName = "BreadcrumbLink";
+
+const BreadcrumbPage = React.forwardRef<
+  HTMLSpanElement,
+  React.ComponentPropsWithoutRef<"span">
+>(({ className, ...props }, ref) => (
+  <span
+    ref={ref}
+    role="link"
+    aria-disabled="true"
+    aria-current="page"
+    className={twMerge("font-normal text-foreground", className)}
+    {...props}
+  />
+));
+BreadcrumbPage.displayName = "BreadcrumbPage";
+
+const BreadcrumbSeparator = ({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"li">) => (
+  <li
+    role="presentation"
+    aria-hidden="true"
+    className={twMerge("[&>svg]:w-3.5 [&>svg]:h-3.5", className)}
+    {...props}
+  >
+    {children ?? <ChevronRightIcon />}
+  </li>
 );
+BreadcrumbSeparator.displayName = "BreadcrumbSeparator";
 
-Breadcrumbs.displayName = "Breadcrumbs";
+//It visually hides the text "More" but keeps it accessible to screen readers, ensuring better accessibility.
+const BreadcrumbEllipsis = ({
+  className,
+  ...props
+}: React.ComponentProps<"span">) => (
+  <span
+    role="presentation"
+    aria-hidden="true"
+    className={twMerge("flex h-9 w-9 items-center justify-center", className)}
+    {...props}
+  >
+    <DotsHorizontalIcon className="h-4 w-4" />
+    <span className="sr-only">More</span>
+  </span>
+);
+BreadcrumbEllipsis.displayName = "BreadcrumbElipssis";
 
-export { Breadcrumbs };
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+};
