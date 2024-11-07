66f0e3342d894d514dcb5285f6a21610a83d3b4a
ser888gio <nica.sergiu@seznam.cz>
2024-11-07 11:19:20 +0100

    Created a multiple select field.

---
+++ packages/design-system/src/ui/input/combobobx/option.tsx
@@ -0,0 +1,22 @@
+import { ComboboxOption as OptionPrimitive } from "@headlessui/react";
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+const Option = forwardRef<
+  React.ElementRef<typeof OptionPrimitive>,
+  React.ComponentPropsWithoutRef<typeof OptionPrimitive>
+>(({ className, children, ...props }, ref) => (
+  <OptionPrimitive
+    ref={ref}
+    className={twMerge(
+      "k1-px-4 k1-py-3 k1-border-b k1-border-neutral k1-justify-start k1-items-start k1-inline-flex hover:k1-bg-neutral-backdrop-hover active:k1-bg-neutral-backdrop-active k1-text-neutral-fg k1-text-base k1-w-full",
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </OptionPrimitive>
+));
+Option.displayName = "Combobox.Option";
+
+export { Option };
