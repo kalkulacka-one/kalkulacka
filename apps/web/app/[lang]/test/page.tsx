c0e465f0b5be5d26b1aa043bfcb78f27f0800aed
martinwenisch <martin@muhu.digital>
2024-12-02 16:07:15 +0100

    Generate ts definition for schema package

--- apps/web/app/[lang]/test/page.tsx
+++ apps/web/app/[lang]/test/page.tsx
@@ -2,8 +2,18 @@
 
 import { BadgeStarIcon } from "@repo/design-system/demo";
 import { InputField } from "@repo/design-system/ui";
+import { type Calculator } from "@repo/schema";
 
 export default function Page() {
+  const test: Calculator = {
+    id: "123",
+    variant: {
+      key: "123",
+    },
+  };
+
+  console.info(test);
+
   return (
     <div className="w-full min-h-80 flex flex-col p-10 justify-center items-center gap-4">
       <InputField label="Full Name" icon={BadgeStarIcon} />
