4b35a8f4175c2b268054b28b5265b73f5b5e0f18
Michał Wierzgoń <michal.wierzgon@gmail.com>
2024-12-02 14:10:15 +0100

    Design system/layouts/question (#66)

---
+++ packages/design-system/config/padding.ts
@@ -0,0 +1,11 @@
+import { ThemeConfig } from "tailwindcss/types/config";
+
+const padding: Pick<Partial<ThemeConfig>, "padding"> = {
+  // We're adding these as a default padding scale, but feel free to use any multiplies of 0.25rem as part of default Tailwind CSS padding scale e.g. h-10 p-2
+  padding: {
+    customDesktop: "calc(calc(1rem * 2) - calc(1rem / 16))",
+    customMobile: "calc(1rem / 2 - 1rem / 16)",
+  },
+};
+
+export default padding;
--- packages/design-system/tailwind.config.ts
+++ packages/design-system/tailwind.config.ts
@@ -4,17 +4,21 @@ import typography from "./config/typography";
 import spacing from "./config/spacing";
 import borderRadius from "./config/borderRadius";
 import boxShadow from "./config/boxShadow";
+import screens from "./config/screens";
+import padding from "./config/padding";
 
 // Each package is responsible for its own content
 const config: Config = {
   content: ["./src/**/*.tsx"],
   prefix: "k1-",
   theme: {
+    ...screens,
     extend: {
       ...colors,
       ...typography,
       ...spacing,
       ...boxShadow,
+      ...padding,
     },
     // Replace the default Tailwind CSS with our own
     ...borderRadius,
