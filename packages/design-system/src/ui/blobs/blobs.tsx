1a6780e1d9e43b89f54d15527de5164ba06b3403
martinwenisch <martin@muhu.digital>
2024-12-03 11:13:08 +0100

    lint fix

--- packages/design-system/src/ui/blobs/blobs.tsx
+++ packages/design-system/src/ui/blobs/blobs.tsx
@@ -3,11 +3,11 @@ import { RedBlob, BlueBlob } from "@repo/design-system/svg";
 const Blobs = () => {
   return (
     // background div - needs top placement
-    <div className="k1-w-full k1-h-full">
+    <div className="k1-h-full k1-w-full">
       {/* blobs div */}
-      <div className="k1-w-full k1-h-full k1-fixed -k1-z-10">
-        <BlueBlob className="k1-fixed k1-left-[5%] k1-top-[10%] k1-h-[80%] k1-blur-[120px]" />
-        <RedBlob className="k1-fixed k1-left-[50%] k1-top-[20%] k1-h-[80%] k1-blur-[120px]" />
+      <div className="k1-fixed -k1-z-10 k1-h-full k1-w-full">
+        <BlueBlob className="k1-fixed k1-left-[5%] k1-top-[10%] k1-h-4/5 k1-blur-[120px]" />
+        <RedBlob className="k1-fixed k1-left-1/2 k1-top-[20%] k1-h-4/5 k1-blur-[120px]" />
       </div>
     </div>
   );
