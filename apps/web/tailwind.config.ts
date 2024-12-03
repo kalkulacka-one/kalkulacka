7ed045d4934694684efa04d7e182388865da4e6d
martinwenisch <martin@muhu.digital>
2024-12-03 10:31:27 +0100

    Remove fonts from design system

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,42 +1,9 @@
 import globalConfig from "@repo/design-system/tailwind";
 import type { Config } from "tailwindcss";
 
-const config: Pick<Config, "content" | "presets" | "prefix" | "theme"> = {
+const config: Pick<Config, "content" | "presets" | "prefix"> = {
   presets: [globalConfig],
   content: ["./app/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        primary: [
-          "var(--font-primary)",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          '"Segoe UI"',
-          "Roboto",
-          '"Helvetica Neue"',
-          "Arial",
-          "sans-serif",
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-        ],
-        secondary: [
-          "var(--font-secondary)",
-          "Georgia",
-          '"Times New Roman"',
-          "Times",
-          "serif",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          '"Segoe UI"',
-          "Roboto",
-          '"Helvetica Neue"',
-          "Arial",
-          "sans-serif",
-        ],
-      },
-    },
-  },
   prefix: "", // design-system has k1- prefix. We might or might not use it in other projects. But it's better to not conflict with it.
 };
 
