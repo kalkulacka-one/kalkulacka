7a60f70cb12264e2a8a93727607548fc3b91cf15
mewdev <michal.wierzgon@gmail.com>
2024-12-09 10:48:16 +0100

    wip: added simple interactivity for steps, needs fix with questionCount

--- packages/design-system/src/ui/layout/bottomBar.tsx
+++ packages/design-system/src/ui/layout/bottomBar.tsx
@@ -2,43 +2,46 @@ import { ButtonInFavour, ButtonAgainst } from "@repo/design-system/ui";
 import { StepProgressFancy } from "@repo/design-system/ui";
 import { StarIconButton } from "@repo/design-system/ui";
 
+type ButtonType = "inFavour" | "against";
+
 type Props = {
-  // solve unused button type problem
-  onClick: (button: string) => void;
+  // solve unused button type eslint problem
+  onClick: (button: ButtonType) => void;
+  steps?: any;
 };
 
-export function BottomBar({ onClick }: Props) {
-  const steps = {
-    answers: [
-      { answerId: "1", status: true }, // positive step (e.g. answerInFavour)
-      { answerId: "2", status: null },
-      { answerId: "3", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
-      { answerId: "4", status: false }, // negative step (e.g. answerAgainst)
-      { answerId: "5", status: true },
-      { answerId: "6", status: true }, //
-      { answerId: "7", status: false },
-      { answerId: "8", status: undefined }, // step with no sratus (e.g. not visited yet)
-      { answerId: "9", status: true }, // positive step (e.g. answerInFavour)
-      { answerId: "10", status: null },
-      { answerId: "11", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
-      { answerId: "12", status: false }, // negative step (e.g. answerAgainst)
-      { answerId: "13", status: true },
-      { answerId: "14", status: true }, //
-      { answerId: "15", status: false },
-      { answerId: "16", status: undefined }, // step with no sratus (e.g. not visited yet)
-      { answerId: "17", status: undefined }, // step with no sratus (e.g. not visited yet)
-      { answerId: "18", status: true }, // positive step (e.g. answerInFavour)
-      { answerId: "19", status: null },
-      { answerId: "20", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
-      { answerId: "21", status: false }, // negative step (e.g. answerAgainst)
-      { answerId: "22", status: true },
-      { answerId: "23", status: true }, //
-      { answerId: "24", status: false },
-      { answerId: "25", status: undefined }, // step with no sratus (e.g. not visited yet)
-    ],
-    totalQuestion: 4,
-    currentQuestion: 8,
-  };
+export function BottomBar({ onClick, steps }: Props) {
+  // const steps = {
+  //   answers: [
+  //     { answerId: "1", status: true }, // positive step (e.g. answerInFavour)
+  //     { answerId: "2", status: null },
+  //     { answerId: "3", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
+  //     { answerId: "4", status: false }, // negative step (e.g. answerAgainst)
+  //     { answerId: "5", status: true },
+  //     { answerId: "6", status: true }, //
+  //     { answerId: "7", status: false },
+  //     { answerId: "8", status: undefined }, // step with no sratus (e.g. not visited yet)
+  //     { answerId: "9", status: true }, // positive step (e.g. answerInFavour)
+  //     { answerId: "10", status: null },
+  //     { answerId: "11", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
+  //     { answerId: "12", status: false }, // negative step (e.g. answerAgainst)
+  //     { answerId: "13", status: true },
+  //     { answerId: "14", status: true }, //
+  //     { answerId: "15", status: false },
+  //     { answerId: "16", status: undefined }, // step with no sratus (e.g. not visited yet)
+  //     { answerId: "17", status: undefined }, // step with no sratus (e.g. not visited yet)
+  //     { answerId: "18", status: true }, // positive step (e.g. answerInFavour)
+  //     { answerId: "19", status: null },
+  //     { answerId: "20", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
+  //     { answerId: "21", status: false }, // negative step (e.g. answerAgainst)
+  //     { answerId: "22", status: true },
+  //     { answerId: "23", status: true }, //
+  //     { answerId: "24", status: false },
+  //     { answerId: "25", status: undefined }, // step with no sratus (e.g. not visited yet)
+  //   ],
+  //   totalQuestion: 4,
+  //   currentQuestion: 8,
+  // };
 
   return (
     // main wrapper
