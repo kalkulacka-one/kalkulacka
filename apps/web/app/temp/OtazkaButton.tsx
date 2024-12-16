f4b4d3a4ae6e6c3392c235e0e69c0f333f1dfbb4
mewdev <michal.wierzgon@gmail.com>
2024-12-16 16:56:56 +0100

    wip: dynamic route updated to have a index number of the question

--- apps/web/app/temp/OtazkaButton.tsx
+++ apps/web/app/temp/OtazkaButton.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-export default function OtazkaButton({ id }: any) {
+export default function OtazkaButton({ index }: any) {
   const router = useRouter();
   function handleClick() {
-    router.push(`/cs/otazky/${id}`);
+    router.push(`/cs/otazky/${index}`);
   }
   return (
     <button className="block bg-blue-600 p-4 text-white" onClick={handleClick}>
