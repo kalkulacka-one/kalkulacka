79dba0cff3e2494e3782a26dd643979a0373acb9
mewdev <michal.wierzgon@gmail.com>
2024-12-19 08:26:18 +0100

    wip: questions pages architecture test continuation

--- apps/web/app/[lang]/otazky/[index]/page.tsx
+++ apps/web/app/[lang]/otazky/[index]/page.tsx
@@ -1,4 +1,5 @@
-import OtazkaComponent from "../../../temp/OtazkaComponent";
+// import OtazkaComponent from "../../../temp/OtazkaComponent";
+import QuestionPageWrapper from "../../../temp/QuestionPageWrapper";
 
 export async function generateStaticParams() {
   const res = await fetch(
@@ -19,13 +20,30 @@ async function getQuestion(index) {
   return data[index];
 }
 
+async function getQuestionsLength() {
+  const res = await fetch(
+    "https://www.volebnikalkulacka.cz/data/instance/volebnikalkulacka.cz/krajske-2024/10-jihomoravsky/questions.json",
+  );
+  const data = await res.json();
+  return data.length;
+}
+
 export default async function Page({ params }) {
   const question = await getQuestion(params.index);
+  const questionLength = await getQuestionsLength();
+  console.log(questionLength);
   return (
-    <OtazkaComponent
-      key={question.id}
-      statement={question.statement}
-      detail={question.detail}
-    />
+    <div>
+      {/* <OtazkaComponent
+        key={question.id}
+        statement={question.statement}
+        detail={question.detail}
+      /> */}
+      <QuestionPageWrapper
+        questions={question}
+        currentQuestion={params.index}
+        questionCount={questionLength}
+      />
+    </div>
   );
 }
