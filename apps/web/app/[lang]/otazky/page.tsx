7bca40922e4d68a6df2dc72df9906bc052a204e4
mewdev <michal.wierzgon@gmail.com>
2024-12-19 08:26:18 +0100

    wip: questions pages architecture test continuation

--- apps/web/app/[lang]/otazky/page.tsx
+++ apps/web/app/[lang]/otazky/page.tsx
@@ -10,38 +10,6 @@ type Props = {
   params: { otazkaId: string };
 };
 
-const steps = {
-  answers: [
-    { answerId: "1", status: true }, // positive step (e.g. answerInFavour)
-    { answerId: "2", status: null },
-    { answerId: "3", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
-    { answerId: "4", status: false }, // negative step (e.g. answerAgainst)
-    { answerId: "5", status: true },
-    { answerId: "6", status: true }, //
-    { answerId: "7", status: false },
-    { answerId: "8", status: undefined }, // step with no sratus (e.g. not visited yet)
-    { answerId: "9", status: true }, // positive step (e.g. answerInFavour)
-    { answerId: "10", status: null },
-    { answerId: "11", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
-    { answerId: "12", status: false }, // negative step (e.g. answerAgainst)
-    { answerId: "13", status: true },
-    { answerId: "14", status: true }, //
-    { answerId: "15", status: false },
-    { answerId: "16", status: undefined }, // step with no sratus (e.g. not visited yet)
-    { answerId: "17", status: undefined }, // step with no sratus (e.g. not visited yet)
-    { answerId: "18", status: true }, // positive step (e.g. answerInFavour)
-    { answerId: "19", status: null },
-    { answerId: "20", status: null }, // neutral step (e.g. visited / skipped, answerNeutral)
-    { answerId: "21", status: false }, // negative step (e.g. answerAgainst)
-    { answerId: "22", status: true },
-    { answerId: "23", status: true }, //
-    { answerId: "24", status: false },
-    { answerId: "25", status: undefined }, // step with no sratus (e.g. not visited yet)
-  ],
-  totalQuestion: 4,
-  currentQuestion: 8,
-};
-
 async function getData() {
   const res = await fetch(
     "https://www.volebnikalkulacka.cz/data/instance/volebnikalkulacka.cz/krajske-2024/10-jihomoravsky/questions.json",
@@ -53,6 +21,7 @@ export default async function Page({ params }: Props) {
   const questions = await getData();
   return (
     <>
+      <span>Next redirect here</span>
       {/* <span>Questions length: {questions.length}</span>
       {questions.length > 0 &&
         questions.map((question: Question, index: number) => {
@@ -67,7 +36,7 @@ export default async function Page({ params }: Props) {
           );
         })}
       <ClientBottomBar steps={steps} /> */}
-      <QuestionPageWrapper questions={questions} />
+      {/* <QuestionPageWrapper questions={questions} /> */}
     </>
   );
 }
