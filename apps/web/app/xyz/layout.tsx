77a350f0215cf4aacb4e05c38ccab57679602d5c
mewdev <michal.wierzgon@gmail.com>
2024-12-23 14:57:55 +0100

    wip: initializer, fetching data and updating the store

--- apps/web/app/xyz/layout.tsx
+++ apps/web/app/xyz/layout.tsx
@@ -1,26 +1,30 @@
 import "../globals.css";
 import "@repo/design-system/styles";
 import "@repo/design-system/themes/theme-default";
-import { useQuestionsStore } from "./store";
+import KalkulackaInitializer from "./KalkulackaInitializer";
 
-async function fetchQuestions() {
-  const res = await fetch(
-    "https://www.volebnikalkulacka.cz/data/instance/volebnikalkulacka.cz/krajske-2024/10-jihomoravsky/questions.json",
-  );
-  const data = await res.json();
-  // console.log(data);
-}
-
-fetchQuestions();
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  async function fetchQuestions() {
+    "use server";
+    const res = await fetch(
+      "https://www.volebnikalkulacka.cz/data/instance/volebnikalkulacka.cz/krajske-2024/10-jihomoravsky/questions.json",
+    );
+    const data = await res.json();
+    return data;
+  }
+
+  const testQuestions = await fetchQuestions();
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <KalkulackaInitializer testQuestions={testQuestions}>
+          {children}
+        </KalkulackaInitializer>
+      </body>
     </html>
   );
 }
