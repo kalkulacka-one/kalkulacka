c37dc8841f28325eab0babc258291c53cbb443fa
mewdev <michal.wierzgon@gmail.com>
2025-01-09 11:47:07 +0100

    wip: implement question navigation and URL updates using Zustand store and browser api

--- apps/web/app/abc/page.tsx
+++ apps/web/app/abc/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useCounterStore } from "./providers/counterStoreProvider";
 
 type Question = {
   id: string;
@@ -11,38 +12,34 @@ type Question = {
 };
 
 export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [questions, setQuestions] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  const questions = useCounterStore((state) => state.questions);
+  const currentQuestion = useCounterStore((state) => state.currentQuestion);
+  const prevQuestion = useCounterStore((state) => state.prevQuestion);
+  const skipQuestion = useCounterStore((state) => state.skipQuestion);
+  // const [questions, setQuestions] = useState([]);
 
-  useEffect(() => {
-    // make a generic fn
-    async function fetchQuestions() {
-      const res = await fetch(
-        "https://www.volebnikalkulacka.cz/data/instance/volebnikalkulacka.cz/krajske-2024/10-jihomoravsky/questions.json",
-      );
-      const data = await res.json();
-      setQuestions(data);
-      setIsLoading(false);
-    }
-    fetchQuestions();
-  }, []);
+  // useEffect(() => {
+  //   // make a generic fn
+  //   async function fetchQuestions() {
+  //     const res = await fetch(
+  //       "https://www.volebnikalkulacka.cz/data/instance/volebnikalkulacka.cz/krajske-2024/10-jihomoravsky/questions.json",
+  //     );
+  //     const data = await res.json();
+  //     setQuestions(data);
+  //     setIsLoading(false);
+  //   }
+  //   fetchQuestions();
+  // }, []);
 
   // loading ui
-  if (isLoading) {
-    return <h1>Data is Loading</h1>;
-  }
+  // if (isLoading) {
+  //   return <h1>Data is Loading</h1>;
+  // }
   // loaded ui
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {questions?.map((question: Question) => {
-          return (
-            <div key={question.id} className="m-4 bg-red-400">
-              <span>{question.statement}</span>
-            </div>
-          );
-        })}
-      </div>
+      <div>Needs redirect</div>
     </main>
   );
 }
--- apps/web/app/abc/store.ts
+++ apps/web/app/abc/store.ts
@@ -2,17 +2,23 @@ import { createStore } from "zustand/vanilla";
 
 export type CounterState = {
   count: number;
+  questions: string[];
+  currentQuestion: number;
 };
 
 export type CounterActions = {
   decrementCount: () => void;
   incrementCount: () => void;
+  prevQuestion: () => void;
+  skipQuestion: () => void;
 };
 
 export type CounterStore = CounterState & CounterActions;
 
 export const defaultInitState: CounterState = {
   count: 0,
+  questions: ["Item 1", "Item 2", "Item 3"],
+  currentQuestion: 1,
 };
 
 export const createCounterStore = (
@@ -22,5 +28,9 @@ export const createCounterStore = (
     ...initState,
     decrementCount: () => set((state) => ({ count: state.count - 1 })),
     incrementCount: () => set((state) => ({ count: state.count + 1 })),
+    prevQuestion: () =>
+      set((state) => ({ currentQuestion: state.currentQuestion - 1 })),
+    skipQuestion: () =>
+      set((state) => ({ currentQuestion: state.currentQuestion + 1 })),
   }));
 };
