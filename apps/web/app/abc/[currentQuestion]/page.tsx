434a2ccb31ccfbccaf761a79c9e0f320897842da
mewdev <michal.wierzgon@gmail.com>
2025-01-09 14:14:56 +0100

    wip: migrated from mockup counter store to questions store, update components and layout accordingly

--- apps/web/app/abc/[currentQuestion]/page.tsx
+++ apps/web/app/abc/[currentQuestion]/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 
-import { useCounterStore } from "../providers/counterStoreProvider";
-
-type Question = {
-  id: string;
-  title: string;
-  statement: string;
-  detail: string;
-  tags: string[];
-};
+import { useQuestionsStore } from "../providers/storeProvider";
 
 export default function Page() {
-  const questions = useCounterStore((state) => state.questions);
-  const currentQuestion = useCounterStore((state) => state.currentQuestion);
-  const prevQuestion = useCounterStore((state) => state.prevQuestion);
-  const skipQuestion = useCounterStore((state) => state.skipQuestion);
+  const questions = useQuestionsStore((state) => state.questions);
+  const collection = useQuestionsStore((state) => state.collection);
+  const currentQuestion = useQuestionsStore((state) => state.currentQuestion);
+  const prevQuestion = useQuestionsStore((state) => state.prevQuestion);
+  const skipQuestion = useQuestionsStore((state) => state.skipQuestion);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="bg-red-200">
+        <h2>Questions</h2>
         {questions?.map((question: string, index) => {
           if (currentQuestion === index + 1) {
             return (
@@ -29,6 +23,19 @@ export default function Page() {
           }
         })}
       </div>
+      <div className="bg-red-200">
+        <h2>Collection</h2>
+        {collection?.map((item: string, index) => {
+          if (currentQuestion === index + 1) {
+            return (
+              <div key={index} className="m-4 bg-red-400">
+                <span>{item}</span>
+              </div>
+            );
+          }
+        })}
+      </div>
+
       <button onClick={skipQuestion}>Skip question</button>
       <button onClick={prevQuestion}>Prev question</button>
     </main>
--- apps/web/app/abc/store.ts
+++ apps/web/app/abc/store.ts
@@ -4,6 +4,7 @@ export type CounterState = {
   count: number;
   questions: string[];
   currentQuestion: number;
+  collection: string[];
 };
 
 export type CounterActions = {
@@ -14,23 +15,3 @@ export type CounterActions = {
 };
 
 export type CounterStore = CounterState & CounterActions;
-
-export const defaultInitState: CounterState = {
-  count: 0,
-  questions: ["Item 1", "Item 2", "Item 3"],
-  currentQuestion: 1,
-};
-
-export const createCounterStore = (
-  initState: CounterState = defaultInitState,
-) => {
-  return createStore<CounterStore>()((set) => ({
-    ...initState,
-    decrementCount: () => set((state) => ({ count: state.count - 1 })),
-    incrementCount: () => set((state) => ({ count: state.count + 1 })),
-    prevQuestion: () =>
-      set((state) => ({ currentQuestion: state.currentQuestion - 1 })),
-    skipQuestion: () =>
-      set((state) => ({ currentQuestion: state.currentQuestion + 1 })),
-  }));
-};
