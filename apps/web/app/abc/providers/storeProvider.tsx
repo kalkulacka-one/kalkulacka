a111631b873c9a194926a925bd13081b95ea06fb
mewdev <michal.wierzgon@gmail.com>
2025-01-10 07:55:40 +0100

    wip: updated question and guide handling in store, enhance data structure and layout

--- apps/web/app/abc/providers/storeProvider.tsx
+++ apps/web/app/abc/providers/storeProvider.tsx
@@ -2,28 +2,37 @@
 
 import { type ReactNode, createContext, useRef, useContext } from "react";
 import { type StoreApi, createStore, useStore } from "zustand";
+import { Question } from "@repo/schema/dist";
+import guide from "../navod/guide.json";
+
+// extend question type like this? (need isImportant, answerType)
+
+// type ExtendedQuestions = Question & {
+//   isImportant: true | false | null;
+//   answerType: true | false | null;
+// };
 
 // divide store, to the external file?
 
 type Store = {
-  questions: string[];
+  questions: Question[];
   currentQuestion: number;
   prevQuestion: () => void;
   skipQuestion: () => void;
   nextGuide: () => void;
   prevGuide: () => void;
   guideNumber: number;
-  guideContent: string[];
+  guide: { message: string }[];
 };
 
 export const StoreContext = createContext<StoreApi<Store> | undefined>(
   undefined,
 );
 
-// update to props with children
+// update to props with children, assign questions differently?
 export interface StoreProviderProps {
   children: ReactNode;
-  questions: string[];
+  questions: Question[];
 }
 
 export const StoreProvider = ({ children, questions }: StoreProviderProps) => {
@@ -37,7 +46,7 @@ export const StoreProvider = ({ children, questions }: StoreProviderProps) => {
       skipQuestion: () =>
         set((state) => ({ currentQuestion: state.currentQuestion + 1 })),
       guideNumber: 1,
-      guideContent: ["Guide item 1", "Guide item 2", "Guide item 3"],
+      guide: guide,
       prevGuide: () => set((state) => ({ guideNumber: state.guideNumber - 1 })),
       nextGuide: () => set((state) => ({ guideNumber: state.guideNumber + 1 })),
     }));
