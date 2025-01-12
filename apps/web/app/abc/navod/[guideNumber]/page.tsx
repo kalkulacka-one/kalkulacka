d71b6272ec20888ee0e8473b55962d5270804f92
mewdev <michal.wierzgon@gmail.com>
2025-01-12 14:59:05 +0100

    wip: guidePage styling improvements, updated navigation arrows for guide page and enhanced  button styles

--- apps/web/app/abc/navod/[guideNumber]/page.tsx
+++ apps/web/app/abc/navod/[guideNumber]/page.tsx
@@ -3,7 +3,11 @@
 import { Button, StepProgress, Blobs } from "@repo/design-system/ui";
 import { useQuestionsStore } from "../../providers/storeProvider";
 import Link from "next/link";
-import { ArrowIconRight } from "@repo/design-system/icons";
+import {
+  ArrowIconLeft,
+  ArrowIconRight,
+  ForwardIcon,
+} from "@repo/design-system/icons";
 
 export default function Page() {
   const guide = useQuestionsStore((state) => state.guide);
@@ -11,144 +15,142 @@ export default function Page() {
   const prevGuide = useQuestionsStore((state) => state.prevGuide);
   const nextGuide = useQuestionsStore((state) => state.nextGuide);
   return (
-    <div className="grid h-screen grid-rows-[1fr_auto]">
-      {/* <Blobs /> */}
-      {/* fix height !!! */}
-      <main className="items-center xs:flex xs:flex-col xs:gap-2 min-[701px]:grid min-[701px]:grid-cols-[1fr_clamp(32rem,_50vw,_48rem)_1fr] sm:grid sm:grid-cols-[1fr_clamp(32rem,_50vw,_48rem)_1fr] sm:gap-8">
-        {/* grid col 1 */}
-        <div className="flex items-center justify-end">
-          {guideNumber === 1 ? null : (
-            <Button onClick={prevGuide} fitContent kind="link">
-              <span className="text-2xl">←</span>
-            </Button>
-          )}
-        </div>
-        {/* grid col 2 */}
-        <div className="flex flex-col">
-          {/* store content */}
-          {/* add as title-m typography component, make content dynamic */}
-          <span className="text-3xl font-bold text-neutral-strong">
-            Krajské volby 2024{" "}
-            <span style={{ fontSize: "smaller" }} className="text-neutral">
-              Jihomoravský kraj
+    <>
+      <Blobs />
+      {/* mobile arrow bar */}
+      <div className="absolute top-0 flex w-dvw justify-between p-2 sm:hidden">
+        <Button
+          hasIcon
+          icon={ArrowIconLeft}
+          iconPosition="left"
+          kind="link"
+          fitContent
+          onClick={prevGuide}
+        />
+        <Button
+          hasIcon
+          icon={ArrowIconRight}
+          iconPosition="right"
+          kind="link"
+          fitContent
+          onClick={nextGuide}
+        />
+      </div>
+      <div className="grid h-screen grid-rows-[1fr_auto]">
+        {/* fix height !!! */}
+        <main className="place-content-center items-center xs:flex xs:flex-col xs:gap-2 min-[701px]:grid min-[701px]:grid-cols-[1fr_clamp(32rem,_50vw,_48rem)_1fr] sm:grid sm:grid-cols-[1fr_clamp(32rem,_50vw,_48rem)_1fr] sm:gap-8">
+          {/* grid col 1 */}
+          {/* empty div for 700 - 767 screen width */}
+          <div className="block sm:hidden"></div>
+          <div className="hidden items-center justify-end sm:flex">
+            {guideNumber === 1 ? null : (
+              <Button
+                hasIcon
+                icon={ArrowIconLeft}
+                iconPosition="left"
+                kind="link"
+                fitContent
+                onClick={prevGuide}
+              />
+            )}
+          </div>
+          {/* grid col 2 */}
+          <div className="flex flex-col gap-4 p-4">
+            {/* store content */}
+            {/* add as title-m typography component, make content dynamic */}
+            <span className="text-3xl font-bold text-neutral-strong">
+              Krajské volby 2024{" "}
+              <span style={{ fontSize: "smaller" }} className="text-neutral">
+                Jihomoravský kraj
+              </span>
             </span>
-          </span>
-          {guide.map((item, index) => {
-            const current = index + 1;
-            if (current === guideNumber) {
-              return <div>{item.message}</div>;
-            }
-          })}
-          <div></div>
-        </div>
-        {/* grid col 3 */}
-        <div className="flex items-center justify-start">
-          {guideNumber === 4 ? null : (
-            <Button onClick={nextGuide} fitContent kind="link">
-              <span className="text-2xl">→</span>
-            </Button>
-          )}
-        </div>
-      </main>
-      <div className="sticky bottom-0 grid justify-center gap-4 bg-blue-200 p-4 sm:bg-transparent md:w-1/4 md:grid-cols-2 md:justify-self-center">
-        {/* grid col 1 */}
-        <div className="justify-self-start ">
-          <span className="font-primary text-xs font-bold uppercase tracking-wide text-neutral-strong">
-            Návod {guideNumber} / {guide.length}
-          </span>
-        </div>
-        {/* grid col 2 */}
-        <div className="content-center justify-self-end">
-          {/* fix type error */}
-          <StepProgress
-            currentQuestion={guideNumber}
-            totalQuestion={guide.length}
-            answers={guide}
-          />
-        </div>
-        {/* grid col 3 */}
-        <div className="col-[1_/_span_2]">
-          {guideNumber !== guide.length ? (
+            {guide.map((item, index) => {
+              const current = index + 1;
+              if (current === guideNumber) {
+                return (
+                  <div className="text-base font-normal text-neutral">
+                    {item.message}
+                  </div>
+                );
+              }
+            })}
+          </div>
+          {/* grid col 3 */}
+          {/* empty div for 700 - 767 screen width */}
+          <div className="block sm:hidden"></div>
+          <div className="hidden items-center justify-start sm:flex">
+            {guideNumber === 4 ? null : (
+              <Button
+                hasIcon
+                icon={ArrowIconRight}
+                iconPosition="right"
+                kind="link"
+                fitContent
+                onClick={nextGuide}
+              />
+            )}
+          </div>
+        </main>
+        {/* bottom bar wrapper */}
+        <div className="sticky bottom-0 grid grid-cols-2 justify-center gap-4 bg-white p-4 min-[701px]:grid min-[701px]:[grid-template-columns:repeat(2,8rem)] sm:bg-transparent md:w-1/4 md:justify-self-center">
+          {/* grid col 1 */}
+          <div className="justify-self-start">
+            <span className="font-primary text-xs font-bold uppercase tracking-wide text-neutral-strong">
+              Návod {guideNumber} / {guide.length}
+            </span>
+          </div>
+          {/* grid col 2 */}
+          <div className="content-center justify-self-end">
+            {/* fix type error */}
+            <StepProgress
+              currentQuestion={guideNumber}
+              totalQuestion={guide.length}
+              answers={guide}
+            />
+          </div>
+          {/* grid col 3 */}
+          <div className="col-[1_/_span_2]">
+            {guideNumber !== guide.length ? (
+              <Button
+                icon={ArrowIconRight}
+                iconPosition="right"
+                onClick={nextGuide}
+                color="neutral"
+                kind="outline"
+              >
+                Další krok
+              </Button>
+            ) : (
+              <Button
+                kind="filled"
+                size="default"
+                color="primary"
+                iconPosition="right"
+                icon={ArrowIconRight}
+                hasIcon
+              >
+                <Link href="/xyz/1">První otázka</Link>
+              </Button>
+            )}
+          </div>
+          {/* grid col 4 */}
+          {/* invisible on div instead of button due to cva conflicts, fix in tw merge ? */}
+          <div
+            className={`col-[1_/_span_2] justify-self-center ${guideNumber === 4 ? "invisible" : null}`}
+          >
             <Button
-              icon={ArrowIconRight}
-              iconPosition="right"
-              onClick={nextGuide}
-              color="neutral"
-              kind="outline"
-            >
-              Další krok
-            </Button>
-          ) : (
-            <Button
-              kind="filled"
-              size="default"
-              color="primary"
-              iconPosition="right"
-              icon={ArrowIconRight}
+              fitContent
+              kind="link"
+              size="auto"
+              icon={ForwardIcon}
               hasIcon
-              compactable
-              wider
+              iconPosition="right"
             >
-              <Link href="/xyz/1">První otázka</Link>
+              Přeskočit návod
             </Button>
-          )}
-        </div>
-        {/* grid col 4 */}
-        <div className="col-[1_/_span_2] justify-self-center ">
-          {guideNumber === 4 ? (
-            <div>Empty</div>
-          ) : (
-            <Button fitContent kind="link">
-              <Link href="/xyz/1">Přeskočit návod ▶▶</Link>
-            </Button>
-          )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-
-// {/* <div className="flex items-center justify-between">
-// {/* add as label typography component */}
-// <span className="font-primary text-xs font-bold uppercase tracking-wide text-neutral-strong">
-//   Návod {guideNumber} / {guide.length}
-// </span>
-// {/* solve type error and answer type general assign? */}
-// <StepProgress
-//   currentQuestion={guideNumber}
-//   totalQuestion={guide.length}
-//   answers={guide}
-// />
-// </div>
-// {guideNumber !== guide.length && (
-// <Button
-//   icon={ArrowIconRight}
-//   iconPosition="right"
-//   onClick={nextGuide}
-//   color="neutral"
-//   fitContent
-//   kind="outline"
-// >
-//   Další krok
-// </Button>
-// )}
-
-// {guideNumber === 4 ? (
-// <Button
-//   kind="filled"
-//   size="default"
-//   color="primary"
-//   iconPosition="right"
-//   icon={ArrowIconRight}
-//   hasIcon
-//   compactable
-//   wider
-//   fitContent
-// >
-//   <Link href="/xyz/1">První otázka</Link>
-// </Button>
-// ) : (
-// <Button fitContent kind="link">
-//   <Link href="/xyz/1">Přeskočit návod ▶▶</Link>
-// </Button>
-// )} */}
