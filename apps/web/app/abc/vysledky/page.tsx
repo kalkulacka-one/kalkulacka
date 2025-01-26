d74d0f88c2174240357b135e64b77fc78b39633c
mewdev <michal.wierzgon@gmail.com>
2025-01-26 16:56:44 +0100

    wip implemented results page layout with ResultCard component and mockupdata

--- apps/web/app/abc/vysledky/page.tsx
+++ apps/web/app/abc/vysledky/page.tsx
@@ -1,3 +1,128 @@
-export default function Page() {
-  return <h1>Výsledky</h1>;
+"use client";
+import {
+  ArrowIconLeft,
+  ArrowIconRight,
+  ShareIcon,
+} from "@repo/design-system/icons";
+import { Button } from "@repo/design-system/ui";
+import Link from "next/link";
+import ResultCard from "./resultCard";
+
+interface Result {
+  partyShort: string;
+  partyLong: string;
+  result: number;
 }
+
+interface Results {
+  results: Result[];
+}
+
+const data: Results[] = [
+  {
+    results: [
+      {
+        partyShort: "STAN+SOL",
+        partyLong: "STAROSTOVÉ PRO JIŽNÍ MORAVU",
+        result: 89,
+      },
+      {
+        partyShort: "Piráti",
+        partyLong: "Česká pirátská strana",
+        result: 89,
+      },
+      {
+        partyShort: "SOM+Nestran2024",
+        partyLong: "STAROSTOVÉ PRO JIŽNÍ MORAVU",
+        result: 30,
+      },
+      {
+        partyShort: "SOM+Nestran2024",
+        partyLong: "STAROSTOVÉ PRO JIŽNÍ MORAVU",
+        result: 15,
+      },
+      {
+        partyShort: "KDU+ODS+TOP 09",
+        partyLong: "SPOLU - ODS, KDU-ČSL, TOP 09",
+        result: 6,
+      },
+    ],
+  },
+];
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="sticky top-0 z-10 grid w-full grid-cols-[auto_1fr_auto] items-center gap-2 bg-white p-2  sm:gap-8 sm:p-8">
+        {/* fix link wrap, should be link in style of a button! */}
+        {/*Link to the last question "current quesiton" */}
+        <div className="flex items-center  justify-self-start">
+          <Link className="flex items-center" href="/abc/rekapitulace">
+            <Button
+              hasIcon
+              icon={ArrowIconLeft}
+              iconPosition="left"
+              kind="link"
+              fitContent
+              size="auto"
+            />
+          </Link>
+        </div>
+
+        {/* replace with typo compoment */}
+        <div className="justify-self-center sm:mr-auto">
+          <h2 className="text-3xl font-bold tracking-snug text-neutral-strong sm:text-5xl">
+            Moje shoda
+          </h2>
+        </div>
+
+        {/* twmerge button fix here */}
+        <div className="flex items-center gap-4 sm:justify-self-end">
+          <Button
+            hasIcon
+            size="auto"
+            kind="link"
+            iconPosition="left"
+            fitContent
+            icon={ShareIcon}
+          >
+            Sdílet
+          </Button>
+          {/* fix link wrap, should be link in style of a button! */}
+          <div></div>
+          <Link className="hidden sm:inline" href="/abc/vysledky">
+            <Button
+              hasIcon
+              kind="filled"
+              size="default"
+              color="primary"
+              iconPosition="right"
+              fitContent
+              icon={ArrowIconRight}
+            >
+              {/* margin or gap? */}
+              Porovnat odpovědi
+            </Button>
+          </Link>
+        </div>
+      </header>
+      <main className="grid grid-cols-1 justify-center gap-4 p-4 min-[701px]:grid-cols-[clamp(32rem,50vw,48rem)]">
+        {/* cards wrapper */}
+        <div className="flex flex-col gap-4 ">
+          {data[0]?.results.map((result, index) => (
+            <ResultCard
+              key={index}
+              cardNumber={index + 1}
+              isFirst={index === 0}
+              results={result}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+// TODO
+// 1. Main layout
+// 2. Fallback layout
---
+++ apps/web/app/abc/vysledky/resultCard.tsx
@@ -0,0 +1,64 @@
+import { Card, ProgressBar } from "@repo/design-system/ui";
+
+type Props = {
+  cardNumber?: number;
+  isFirst?: boolean;
+  results?: any;
+};
+
+export default function ResultCard({ cardNumber, isFirst, results }: Props) {
+  const { partyShort, partyLong, result } = results;
+
+  return (
+    <Card
+      shadow="default"
+      corner="topLeft"
+      className="flex max-w-full items-center gap-2 p-customMobile sm:p-customDesktop"
+    >
+      {/* avatar */}
+      <div className="flex items-center justify-center">
+        {/* circle */}
+        <div
+          className={` ${isFirst ? "size-10 bg-primary-strong min-[700px]:size-[calc(3*1.5rem)]" : "size-[calc(2*1rem)] bg-primary min-[700px]:size-14"} flex items-center justify-center rounded-full`}
+        >
+          <p
+            className={`${isFirst ? "text-base text-white min-[700px]:text-xl" : "text-sm text-neutral min-[700px]:text-base"} font-semibold min-[700px]:text-xl`}
+          >
+            {cardNumber}.
+          </p>
+        </div>
+      </div>
+      {/* party, percents wrapper */}
+      <div className="flex flex-col gap-4">
+        {/* name short */}
+        <div>
+          <strong>
+            <p
+              className={`${isFirst ? "text-base min-[700px]:text-3xl " : "text-sm min-[700px]:text-base"} font-bold text-neutral  min-[700px]:text-black`}
+            >
+              {partyShort}
+            </p>
+          </strong>
+        </div>
+        {/* progress bar */}
+        <ProgressBar progress={result} />
+        {/* party wrapper */}
+        <div>
+          <p className="text-neutral">{partyLong}</p>
+        </div>
+      </div>
+
+      {/* percent wrapper */}
+      <p
+        className={`${isFirst ? "text-3xl min-[700px]:text-5xl " : "text-base min-[700px]:text-3xl"} ml-auto  font-bold text-neutral-strong`}
+      >
+        {result}%
+      </p>
+    </Card>
+  );
+}
+
+// TODO:
+// 1. Fix custom paddings behaviour on card (desktop)
+// 2. Simplify structure (toggle behaviour content order), if possible
+// 3. Percentage problem fix!
