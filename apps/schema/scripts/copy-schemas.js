06d253dc94260e209a7cb07f98e1b439219d413d
Kryštof Korb <krystof@korb.cz>
2025-02-15 13:32:17 +0100

    Add schema docs app (#80)

---
+++ apps/schema/scripts/copy-schemas.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import schemas from "@repo/schema";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packageRoot = path.resolve(__dirname, "..");
+const buildDir = path.join(packageRoot, "build");
+
+fs.mkdirSync(buildDir, { recursive: true });
+
+Object.entries(schemas).forEach(([schemaName, schemaData]) => {
+  fs.writeFileSync(
+    path.join(buildDir, `${schemaName}.schema.json`),
+    JSON.stringify(schemaData, null, 2),
+  );
+});
---
+++ apps/schema/scripts/generate-openapi.js
@@ -0,0 +1,60 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import yaml from "yaml";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packageRoot = path.resolve(__dirname, "..");
+const buildDir = path.join(packageRoot, "build");
+const outputFile = path.join(buildDir, "openapi.yaml");
+
+const schemaFiles = fs
+  .readdirSync(buildDir)
+  .filter((file) => file.endsWith(".json"));
+
+const version = JSON.parse(
+  fs.readFileSync(
+    path.resolve(
+      path.dirname(fileURLToPath(import.meta.resolve("@repo/schema"))),
+      "package.json",
+    ),
+    "utf-8",
+  ),
+).version;
+
+const openapiDoc = {
+  openapi: "3.1.0",
+  info: {
+    title: "Kalkulacka.1 schemas",
+    version,
+    description: "Schemas of data for voting advice applications.",
+  },
+  tags: [],
+  components: { schemas: {} },
+};
+
+schemaFiles.sort().forEach((file) => {
+  const filePath = path.join(buildDir, file);
+  const schemaContent = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+  const schemaName = path.basename(file, ".schema.json");
+  openapiDoc.components.schemas[schemaName] = { $ref: `./${file}` };
+
+  const name = schemaContent.title;
+  let description = `Schema URL: [${schemaContent.$id}](${schemaContent.$id})\n\n`;
+  description += schemaContent.description
+    ? `${schemaContent.description}\n\n`
+    : "";
+  description += `<SchemaDefinition schemaRef="#/components/schemas/${schemaName}" />`;
+
+  openapiDoc.tags.push({
+    name,
+    description,
+  });
+});
+
+const yamlContent = yaml.stringify(openapiDoc);
+
+fs.writeFileSync(outputFile, yamlContent, "utf8");
