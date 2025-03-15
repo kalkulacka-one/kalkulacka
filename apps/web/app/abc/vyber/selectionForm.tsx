ca5a6f434a6aae095b26ce0750c1679fc865bc69
mewdev <michal.wierzgon@gmail.com>
2025-03-15 16:11:34 +0100

    wip: headless radio group + react hook forms

--- apps/web/app/abc/vyber/selectionForm.tsx
+++ apps/web/app/abc/vyber/selectionForm.tsx
@@ -1,34 +1,49 @@
 import { Button } from "@repo/design-system/ui";
 import districts from "./districts.json";
 import { useForm } from "react-hook-form";
+import { Field, Label, Radio, RadioGroup } from "@headlessui/react";
+import { useState } from "react";
 
 export default function SelectionForm() {
-  const { handleSubmit } = useForm();
+  const [selected, setSelected] = useState("");
+  const {
+    handleSubmit,
+    register,
+    formState: { isDirty },
+  } = useForm({
+    defaultValues: {
+      radio: "",
+    },
+  });
   const onSubmit = (data: any) => console.log(data);
+
+  console.log(selected);
+  console.log(`Dirty state: ${isDirty}`);
+
   return (
     <div className="grid grid-cols-[1fr] justify-center p-4 min-[701px]:grid-cols-[minmax(24em,max-content)]">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col">
-          {districts.map((district) => {
-            return (
-              <label
-                key={district.value}
-                className="group relative flex cursor-pointer items-center border-b border-neutral px-4 py-3"
-              >
-                <input
-                  name="selection"
-                  type="radio"
-                  className="peer hidden"
+        <RadioGroup
+          value={selected}
+          onChange={setSelected}
+          // dynamic here !!
+          aria-label="Zvolte svůj kraj"
+        >
+          {districts.map((district) => (
+            <Field key={district.value} {...register("radio")}>
+              {/* blue focus fix! */}
+              <Label className="group relative flex cursor-pointer items-center border-b border-neutral px-4 py-3">
+                <Radio
                   value={district.value}
-                />
-                <span className="relative flex size-6 items-center justify-center rounded-full border-2 border-solid border-neutral-strong group-hover:border-neutral peer-checked:after:absolute peer-checked:after:size-[0.6rem] peer-checked:after:rounded-full peer-checked:after:bg-[#565252] peer-checked:after:content-[''] group-hover:peer-checked:after:bg-[#ADA6A6]"></span>
-                <span className="ml-4">
-                  <p className="text-neutral">{district.label}</p>
-                </span>
-              </label>
-            );
-          })}
-        </div>
+                  className="!focus:outline-none group flex size-6 items-center justify-center rounded-full border-2 border-neutral-strong bg-white"
+                >
+                  <span className="invisible size-[0.6rem] rounded-full bg-[#565252] group-hover:bg-[#ADA6A6] group-data-[checked]:visible" />
+                </Radio>
+                <span className="ml-4 text-neutral">{district.label}</span>
+              </Label>
+            </Field>
+          ))}
+        </RadioGroup>
         <div className="sticky bottom-0 flex w-full justify-center justify-self-center bg-white p-4  sm:bg-transparent">
           <Button
             type="submit"
@@ -36,6 +51,7 @@ export default function SelectionForm() {
             size="default"
             color="primary"
             fitContent
+            disabled={!selected ? true : false}
           >
             Potvrdit a pokračovat
           </Button>
