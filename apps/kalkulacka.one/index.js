248cf8bcd680e88540f23e98e29b887f2b0907b1
Kryštof Korb <krystof@korb.cz>
2025-03-17 23:40:06 +0100

    Update worker to use JSON media type for Matrix

--- apps/kalkulacka.one/index.js
+++ apps/kalkulacka.one/index.js
@@ -2,6 +2,13 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
+    if (url.pathname.startsWith('/.well-known/matrix')) {
+      let response = await env.ASSETS.fetch(request);
+      response = new Response(response.body, response);
+      response.headers.set('Content-Type', 'application/json');
+      return response;
+    }
+
     url.hostname = `www.${url.hostname}`;
     return Response.redirect(url.toString(), 301);
   },
