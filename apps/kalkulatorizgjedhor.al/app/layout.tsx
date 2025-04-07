08ab8dc8e86be4f6324a2809284f0dc2254caec3
mewdev <michal.wierzgon@gmail.com>
2025-04-07 09:25:29 +0200

    wip: testing base project setup i18n with react-intl

--- apps/kalkulatorizgjedhor.al/app/layout.tsx
+++ apps/kalkulatorizgjedhor.al/app/layout.tsx
@@ -1,12 +1,21 @@
 import '@repo/design-system/styles';
 import '@repo/design-system/themes/theme-default';
 import type { Metadata } from 'next';
-
+import English from '../i18n/en.json';
+import Albanian from '../i18n/sq.json';
+import ServerIntlProvider from './serverIntlProvider';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Kalkulatori zgjedhor',
   description: 'Kalkulatori zgjedhor është një projekt i organizatës jofitimprurëse KohoVolit.eu, në bashkëpunim me Faktoje, dhe është një ndihmës i paanshëm në vendimin tuaj se kë të votoni.',
+};
+
+const locale = 'sq';
+
+const messages = {
+  en: English,
+  sq: Albanian,
 };
 
 export default function RootLayout({
@@ -15,13 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="sq">
-      <head>
-        <script defer data-domain="kalkulatorizgjedhor.al" src="/js/script.tagged-events.outbound-links.js" />
-      </head>
-      <body>
-        <main className="relative grid h-screen p-4">{children}</main>
-      </body>
-    </html>
+    <ServerIntlProvider defaultLocale={locale} locale={locale} messages={messages[locale]}>
+      <html lang={locale}>
+        <head>
+          <script defer data-domain="kalkulatorizgjedhor.al" src="/js/script.tagged-events.outbound-links.js" />
+        </head>
+        <body>
+          <main className="relative grid h-screen p-4">{children}</main>
+        </body>
+      </html>
+    </ServerIntlProvider>
   );
 }
--- apps/kalkulatorizgjedhor.al/app/page.tsx
+++ apps/kalkulatorizgjedhor.al/app/page.tsx
@@ -1,3 +1,15 @@
-export default async function Page() {
-  return <div>Kalkulatori zgjedhor</div>;
+'use client';
+import { FormattedMessage } from 'react-intl';
+export default function Page() {
+  return (
+    <div>
+      <h2>Page</h2>
+      <p>
+        <FormattedMessage id="key1" defaultMessage="Good morning" />
+      </p>
+      <p>
+        <FormattedMessage id="key2" defaultMessage="This message is in English" />
+      </p>
+    </div>
+  );
 }
