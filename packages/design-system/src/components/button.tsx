4f1723540d9a2ec7763603835e25d6f45ee3de63
mewdev <michal.wierzgon@gmail.com>
2025-04-19 11:58:05 +0200

    refactor: button props inline destruction refactor

--- packages/design-system/src/components/button.tsx
+++ packages/design-system/src/components/button.tsx
@@ -1,24 +1,22 @@
-import { Button as HeadlessButton } from '@headlessui/react';
+import * as Headless from '@headlessui/react';
 import React from 'react';
 
 type ButtonProps = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-function ButtonComponent(props: ButtonProps, ref: React.Ref<HTMLButtonElement>) {
-  const { children, ...rest } = props;
+function ButtonComponent({ children, ...props }: ButtonProps, ref: React.Ref<HTMLButtonElement>) {
   return (
-    <HeadlessButton
-      className="ko:flex ko:items-center ko:select-none ko:data-[hover]:cursor-pointer ko:leading-[1.85] ko:tracking-[.07em] ko:uppercase ko:border-2 ko:border-black ko:font-bold ko:p-4 ko:text-sm ko:rounded-2xl ko:rounded-tr-none ko:font-display"
+    <Headless.Button
+      className="ko:select-none ko:data-[hover]:cursor-pointer ko:tracking-[.07em] ko:leading-[1.85] ko:uppercase ko:border-2 ko:border-black ko:font-bold ko:p-4 ko:text-sm ko:rounded-2xl ko:rounded-tr-none ko:font-display"
       ref={ref}
-      {...rest}
+      {...props}
     >
       {children}
-    </HeadlessButton>
+    </Headless.Button>
   );
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(ButtonComponent);
-Button.displayName = 'Button';
+const Button = React.forwardRef(ButtonComponent);
 
 export { Button };
