3f5e6192cdf8348c1ce7ec7fe1ba37448d36d7d6
mewdev <michal.wierzgon@gmail.com>
2025-04-27 15:11:15 +0200

    refactor: added state styling and basic cva config

--- packages/design-system/src/components/button.tsx
+++ packages/design-system/src/components/button.tsx
@@ -1,17 +1,33 @@
 import * as Headless from '@headlessui/react';
+import { type VariantProps, cva } from 'class-variance-authority';
 import React from 'react';
+
+const ButtonStyles = cva(
+  [
+    'ko:data-disabled:bg-gray-400 ko:data-disabled:border-gray-400 ko:data-disabled:cursor-not-allowed ko:data-disabled:data-hover:text-white',
+    'ko:p-4',
+    'ko:select-none ko:data-hover:cursor-pointer',
+    'ko:font-display ko:font-bold ko:uppercase ko:leading-[1.85] ko:tracking-[.07em]',
+    'ko:rounded-tr-none ko:rounded-2xl',
+    'ko:text-xs',
+  ],
+  {
+    variants: {
+      kind: {
+        filled: ['ko:bg-black', 'ko:text-white', 'ko:data-hover:bg-white ko:data-hover:text-black', 'ko:border-2', 'ko:border-black'],
+      },
+    },
+  },
+);
 
 type ButtonProps = {
   children: React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+} & React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof ButtonStyles>;
 
-function ButtonComponent({ children, ...props }: ButtonProps, ref: React.Ref<HTMLButtonElement>) {
+function ButtonComponent({ children, kind, ...props }: ButtonProps, ref: React.Ref<HTMLButtonElement>) {
   return (
-    <Headless.Button
-      className="ko:select-none ko:data-[hover]:cursor-pointer ko:tracking-[.07em] ko:leading-[1.85] ko:uppercase ko:border-2 ko:border-black ko:font-bold ko:p-4 ko:text-sm ko:rounded-2xl ko:rounded-tr-none ko:font-display"
-      ref={ref}
-      {...props}
-    >
+    <Headless.Button className={ButtonStyles({ kind })} ref={ref} {...props}>
       {children}
     </Headless.Button>
   );
