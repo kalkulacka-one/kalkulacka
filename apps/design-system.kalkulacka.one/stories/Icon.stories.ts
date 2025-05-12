502c0527e48497efcd097702400e729df9283c6a
mewdev <michal.wierzgon@gmail.com>
2025-05-12 12:16:41 +0200

    refactor: advanced icon component

---
+++ apps/design-system.kalkulacka.one/stories/Icon.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { mdiAccount } from '@mdi/js';
+import { Icon } from '@repo/design-system/components';
+import { EnvelopeIcon } from '@repo/design-system/icons';
+
+const meta: Meta<typeof Icon> = {
+  title: 'Components/Icon',
+  component: Icon,
+  tags: ['autodocs'],
+  argTypes: {
+    decorative: { control: 'boolean' },
+    title: { control: 'text' },
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large'],
+    },
+  },
+};
+
+type IconStory = StoryObj<typeof Icon>;
+
+export const Decorative: IconStory = {
+  args: {
+    decorative: true,
+    icon: mdiAccount,
+    size: 'medium',
+  },
+};
+
+export const NonDecorative: IconStory = {
+  args: {
+    decorative: false,
+    title: 'Envelope icon',
+    icon: EnvelopeIcon,
+    size: 'medium',
+  },
+};
+
+export default meta;
