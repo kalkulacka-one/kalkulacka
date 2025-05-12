b9eb1656eeb7bed58cbba0fb9d779053d17438be
mewdev <michal.wierzgon@gmail.com>
2025-05-12 12:16:41 +0200

    refactor: advanced icon component

--- packages/design-system/src/components/server/icon.tsx
+++ packages/design-system/src/components/server/icon.tsx
@@ -1,38 +1,55 @@
 import { type VariantProps, cva } from "class-variance-authority";
-import React from "react";
-import type { IconType } from "../../types/icon-types";
-import * as Icons from "./icons";
+import { twMerge } from "tailwind-merge";
 
-export const iconVariants = {
+type SvgIconProps = {
+  title?: string;
+} & React.SVGProps<SVGSVGElement>;
+
+type SvgIcon = React.FunctionComponent<SvgIconProps>;
+
+type BaseIconProps = {
+  icon: string | SvgIcon;
+} & VariantProps<typeof IconStyles> &
+  React.SVGProps<SVGSVGElement>;
+
+type ConditionalIconProps =
+  | {
+      title: string;
+      decorative: false;
+    }
+  | { title?: string; decorative: true };
+
+type IconProps = BaseIconProps & ConditionalIconProps;
+
+const IconStyles = cva("", {
   variants: {
     size: {
       small: "ko:size-4",
       medium: "ko:size-6",
       large: "ko:size-8",
-      extraLarge: "ko:size-10",
-      extraHuge: "ko:size-14",
     },
   },
-} as const;
-
-const iconStyles = cva("", {
-  variants: iconVariants.variants,
   defaultVariants: {
     size: "medium",
   },
 });
-
-type IconVariants = VariantProps<typeof iconStyles>;
-type IconSize = IconVariants["size"];
-export const iconSizes = Object.keys(iconVariants.variants.size) as IconSize[];
-
-type IconProps = {
-  name: IconType;
-  title: string;
-} & VariantProps<typeof iconStyles>;
-
-export function Icon({ name, size, title, ...props }: IconProps) {
-  const className = iconStyles({ size });
-  const IconComponent = React.createElement(Icons[name], { ...props, title, className });
-  return IconComponent;
+export function Icon({ icon, size, title, decorative, className, ...props }: IconProps) {
+  if (typeof icon === "string") {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden={decorative ? "true" : "false"}
+        focusable="false"
+        role="img"
+        className={twMerge(IconStyles({ size }), className)}
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        {title && <title>{title}</title>}
+        <path d={icon} fill="currentColor" />
+      </svg>
+    );
+  }
+  const SvgIcon = icon;
+  return <SvgIcon title={title} focusable="false" role="img" aria-hidden={decorative ? "true" : "false"} {...props} className={twMerge(IconStyles({ size }), className)} />;
 }
