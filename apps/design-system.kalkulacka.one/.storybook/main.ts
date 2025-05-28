19227a3de9b2bb7f69e08faafe219d8d184db872
Kryštof Korb <krystof@korb.cz>
2025-05-28 15:13:13 +0200

    Upgrade to Storybook 9 RC (#119)
    Co-authored-by: martinwenisch <martin@muhu.digital>

--- apps/design-system.kalkulacka.one/.storybook/main.ts
+++ apps/design-system.kalkulacka.one/.storybook/main.ts
@@ -13,13 +13,7 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-interactions'),
-  ],
+  addons: [getAbsolutePath('@storybook/addon-onboarding'), getAbsolutePath('@storybook/addon-links'), getAbsolutePath('@chromatic-com/storybook'), getAbsolutePath('@storybook/addon-docs')],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
     options: {},
