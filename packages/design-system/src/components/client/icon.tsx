855dd157faff80906712aac5d907420e5466b5ba
mewdev <michal.wierzgon@gmail.com>
2025-05-29 17:13:01 +0300

    refactor: general code cleanup, aria role refined, component props naming, story args refine,  biome config quotes including file changes

---
+++ packages/design-system/src/components/client/icon.tsx
@@ -0,0 +1,65 @@
+import { type VariantProps, cva } from "class-variance-authority";
+import { useId } from "react";
+import { twMerge } from "tailwind-merge";
+
+export type Icon = {
+  icon: string | React.FunctionComponent<{ title?: string; titleId?: string; decorative?: boolean } & React.SVGProps<SVGSVGElement>>;
+} & VariantProps<typeof IconVariants> &
+  React.SVGProps<SVGSVGElement> &
+  ({ title: string; decorative: false } | { title?: string; decorative: true });
+
+const IconVariants = cva("", {
+  variants: {
+    size: {
+      small: "ko:size-4",
+
+      medium: "ko:size-6",
+
+      large: "ko:size-8",
+    },
+  },
+
+  defaultVariants: {
+    size: "medium",
+  },
+});
+
+export function Icon({ icon, size, title, decorative, className, ...props }: Icon) {
+  const titleId = useId();
+
+  if (typeof icon === "string") {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden={decorative ? "true" : "false"}
+        aria-labelledby={!decorative ? titleId : undefined}
+        focusable="false"
+        role={decorative ? undefined : "img"}
+        className={twMerge(IconVariants({ size }), className)}
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        {!decorative && title && <title id={titleId}>{title}</title>}
+
+        <path d={icon} fill="currentColor" />
+      </svg>
+    );
+  }
+
+  const SvgIcon = icon;
+
+  return (
+    <SvgIcon
+      {...props}
+      title={title}
+      titleId={titleId}
+      decorative={decorative}
+      focusable="false"
+      role={decorative ? undefined : "img"}
+      aria-labelledby={!decorative ? titleId : undefined}
+      aria-hidden={decorative ? "true" : "false"}
+      className={twMerge(IconVariants({ size }), className)}
+    />
+  );
+}
--- packages/design-system/src/types/icon-types.ts
+++
@@ -1,6 +0,0 @@
-import * as Icons from "../components/server/icons";
-
-export type IconType = keyof typeof Icons;
-
-// A derived list of icon names used for tooling (e.g., Storybook dropdowns)
-export const iconNames = Object.keys(Icons) as IconType[];
