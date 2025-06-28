e5b55cd543c782b548b97507a86fbeea1e088bc6
mewdev <michal.wierzgon@gmail.com>
2025-06-28 08:54:10 +0200

    feat: update internationalization messages and improve layout components, add clientTest dummy intl test

--- apps/www.kalkulacka.one/app/[lang]/layout.tsx
+++ apps/www.kalkulacka.one/app/[lang]/layout.tsx
@@ -1,8 +1,9 @@
 import { IntlProvider } from "@repo/design-system/client";
-import { ClientTest } from "@repo/design-system/client";
+// import { ClientTest } from "@repo/design-system/client";
 import Link from "next/link";
 import getIntl from "../server/getIntl";
 import "../globals.css";
+
 type Params = Promise<{ lang: string }>;
 
 export default async function RootLayout({
@@ -16,16 +17,13 @@ export default async function RootLayout({
 
   const intl = await getIntl(lang);
 
+  console.log(intl);
+
   // TODO:
-  // 1. Translate all content to EN and CS (how default)
-  // 2. Translation keys formatting
-  // 3. Apply all keys for the relevant content
   // 4. Translation keys tests / types (look up formatjs/cli)
   // 5. hooks in client components and memoization
   // 6. Fallbacks
   // 7. Switcher
-
-  console.log(intl);
 
   return (
     <IntlProvider locale={intl.locale} messages={intl.messages}>
@@ -38,36 +36,36 @@ export default async function RootLayout({
             <header className="grid gap-2">
               <Link href="/cs">
                 <h1 className="text-5xl font-semibold">{intl.formatMessage({ id: "header.title" })}</h1>
-                <ClientTest />
-                <p>
-                  Server component:
-                  {intl.formatMessage({
-                    id: "componentTest",
-                    defaultMessage: "Fallback message",
-                  })}
-                </p>
-                <p className="text-lg font-medium">
-                  Ta <em>pravá</em> volební kalkulačka pro miliony voličů ve 4 zemích
-                </p>
+                <p className="text-lg font-medium">{intl.formatMessage({ id: "header.tagline" }, { em: (chunks) => <em>{chunks}</em> })}</p>
               </Link>
               <nav className="flex flex-wrap gap-2">
                 <Link href="/cs" className="underline hover:no-underline">
-                  Domů
+                  {intl.formatMessage({
+                    id: "navigation.home",
+                  })}
                 </Link>
                 <Link href="/cs/podporte-kalkulacku" className="underline hover:no-underline">
-                  Podpora
+                  {intl.formatMessage({
+                    id: "navigation.support",
+                  })}
                 </Link>
                 <Link href="/cs/zapojte-se" className="underline hover:no-underline">
-                  Dobrovolnictví
+                  {intl.formatMessage({
+                    id: "navigation.volunteer",
+                  })}
                 </Link>
                 <Link href="/cs/vlastni-kalkulacka" className="underline hover:no-underline">
-                  Vlastní kalkulačka
+                  {intl.formatMessage({
+                    id: "navigation.customCalculator",
+                  })}
                 </Link>
               </nav>
               <div className="flex flex-wrap gap-2">
                 →
                 <Link href="https://x.com/kalkulacka_one" className="underline hover:no-underline">
-                  @kalkulacka_one na X
+                  {intl.formatMessage({
+                    id: "social.twitterLink",
+                  })}
                 </Link>
               </div>
             </header>
