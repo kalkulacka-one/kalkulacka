5ba83ff3b11b3269066fce860f08d1a57b43bf5a
Kryštof Korb <krystof@korb.cz>
2025-07-08 17:08:53 +0200

    Reorder classes

--- packages/design-system/src/components/client/logo.tsx
+++ packages/design-system/src/components/client/logo.tsx
@@ -9,7 +9,7 @@ export function Logo({ title, text }: Logo) {
   const titleId = useId();
 
   return (
-    <div className="ko:items-center ko:flex ko:gap-[1.78125rem]">
+    <div className="ko:flex ko:items-center ko:gap-[1.78125rem]">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden={text ? "true" : undefined}
