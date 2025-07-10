40820a56e8984049082dad17b12d60ad8085f687
mewdev <michal.wierzgon@gmail.com>
2025-07-10 13:12:12 +0200

    Add initial layout tests for RootLayout component

---
+++ apps/www.volebnikalkulacka.cz/app/layout.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Layout from "./layout";
+
+describe("RootLayout", () => {
+  it("should render the main heading", async () => {
+    const mockParams = Promise.resolve({ lang: "cs" });
+
+    render(await Layout({ params: mockParams, children: null }));
+    expect(screen.getByText("Header"));
+  });
+});
