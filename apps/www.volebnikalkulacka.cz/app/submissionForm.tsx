475eb455ebd9af70bff9355c81a8584ae089bfda
mewdev <michal.wierzgon@gmail.com>
2025-07-13 11:31:49 +0200

    Implement subscription functionality with validation and error handling

--- apps/www.volebnikalkulacka.cz/app/submissionForm.tsx
+++ apps/www.volebnikalkulacka.cz/app/submissionForm.tsx
@@ -3,43 +3,57 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Description, Field, Input, Label } from "@repo/design-system/client";
 import { type SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
-
-const subscribeBodySchema = z.object({
-  email: z.string().email("Neplatný e-mail"),
-});
-
-type SubscribeBody = z.infer<typeof subscribeBodySchema>;
+import { subscribe } from "../src/server/subscribe";
+import { type SubscribeBody, subscribeBodySchema } from "../src/types/subscribe";
 
 export default function SubmissionForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    reset,
+    setError,
+    setFocus,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm<SubscribeBody>({
-    defaultValues: { email: "" },
     resolver: zodResolver(subscribeBodySchema),
   });
 
-  const onSubmit: SubmitHandler<SubscribeBody> = (data) => {
-    console.log("Form submitted:", data);
+  const onSubmit: SubmitHandler<SubscribeBody> = async (data) => {
+    try {
+      const response = await subscribe(data);
+      if (response.success) {
+        reset();
+      } else if (response.error.code === "duplicate-email") {
+      }
+    } catch (error) {
+      setError("root.serverError", {
+        type: "400",
+        message: "Chyba na serveru, kontaktujte vývojáře aplikace.",
+      });
+      setFocus("email");
+    }
   };
 
   return (
-    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)} noValidate>
-      <div className="flex gap-4 justify-center">
-        <Field className="flex gap-4 relative">
-          <Label className="hidden" htmlFor="email">
-            Zadejte svůj email
-          </Label>
-          <Input type="email" placeholder="Zadejte svůj e-mail" {...register("email")} />
-          {errors.email && <Description className="absolute -bottom-1.5 right-4 px-1 bg-white text-xs text-red-500">{errors.email.message}</Description>}
-        </Field>
-        <Button type="submit" variant="outline" color="primary">
-          Odeslat
-        </Button>
-      </div>
-      <p className="text-sm leading-[1.23]">Odesláním souhlasíte se zasíláním novinek o volební kalkulačce.</p>
-    </form>
+    <>
+      {isSubmitSuccessful ? (
+        <div>Děkujeme za vyplnění</div>
+      ) : (
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)} noValidate>
+          <div className="flex gap-4 justify-center">
+            <Field className="flex gap-4 relative" disabled={isSubmitting}>
+              <Label className="sr-only hidden">Zadejte váš email</Label>
+              <Input invalid={errors.email ? true : undefined} autoComplete="email" type="email" placeholder="E-mail" {...register("email")} />
+              {errors.email && <Description className="absolute -bottom-1.5 right-4 px-1 bg-white text-xs text-red-500">{errors.email.message}</Description>}
+            </Field>
+            <Button disabled={isSubmitting} type="submit" variant="outline" color="primary">
+              {isSubmitting ? "Odesílám" : "Odeslat"}
+            </Button>
+            {errors.root?.serverError && <Description className="text-sm">⚠️ {errors.root?.serverError.message}</Description>}
+          </div>
+          <p className="text-sm leading-[1.23]">Odesláním souhlasíte se zasíláním novinek o volební kalkulačce.</p>
+        </form>
+      )}
+    </>
   );
 }
