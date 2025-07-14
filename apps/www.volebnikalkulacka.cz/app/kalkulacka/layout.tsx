d95eba33ca05b0668292630f110f616c480aae1e
mewdev <michal.wierzgon@gmail.com>
2025-07-14 13:38:00 +0200

    Add Kalkulačka app layout and modify structure

---
+++ apps/www.volebnikalkulacka.cz/app/kalkulacka/layout.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@repo/design-system/client";
+import "./../globals.css";
+import Link from "next/link";
+
+export default async function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="cs">
+      <body>
+        <div className="container mx-auto p-16 flex flex-col min-h-screen">
+          <header className="h-14 flex items-center justify-between">
+            <div>Logo</div>
+            <div>Parlamentní volby 2025</div>
+            <Link href="/">
+              <Button variant="link" color="neutral">
+                Zpět na hlavní stránku
+              </Button>
+            </Link>
+          </header>
+          <div className="flex flex-grow items-center justify-center">
+            <main>{children}</main>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
