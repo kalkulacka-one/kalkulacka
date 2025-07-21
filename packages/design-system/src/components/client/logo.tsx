82fbad49c130373c84a09b8bbe6e12373b32502e
mewdev <michal.wierzgon@gmail.com>
2025-07-21 08:54:15 +0200

    Refactor Logo with size variant and separate svg

--- packages/design-system/src/components/client/logo.tsx
+++ packages/design-system/src/components/client/logo.tsx
@@ -1,50 +1,35 @@
+import { LogoSvg } from "@repo/design-system/svg";
+import { twMerge } from "@repo/design-system/utils";
+import { type VariantProps, cva } from "class-variance-authority";
 import { useId } from "react";
 
 export type Logo = {
   title: string;
   text?: boolean;
-};
+} & VariantProps<typeof LogoVariants>;
 
-export function Logo({ title, text }: Logo) {
+const LogoVariants = cva(
+  ["ko:flex ko:items-center ko:gap-[1.78125rem]", "ko:[&_div]:font-display ko:[&_div]:font-bold ko:[&_div]:uppercase ko:[&_div]:tracking-[10%] ko:[&_div]:leading-[normal] ko:[&_div]:text-nowrap"],
+  {
+    variants: {
+      size: {
+        small: ["ko:[&_svg]:w-[5.953rem] ko:[&_svg]:h-5", "ko:[&_div]:text-xs"],
+        default: ["ko:[&_svg]:h-[4rem]", "ko:[&_div]:text-[2.25rem]"],
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  },
+);
+
+export function Logo({ title, text, size }: Logo) {
   const titleId = useId();
 
   return (
-    <div className="ko:flex ko:items-center ko:gap-[1.78125rem]">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden={text ? "true" : undefined}
-        aria-labelledby={!text ? titleId : undefined}
-        focusable="false"
-        role={text ? undefined : "img"}
-        className="ko:h-[4rem] ko:shrink-0"
-        viewBox="0 0 300 64"
-      >
-        {!text && <title id={titleId}>{title}</title>}
-        <path
-          fill="currentColor"
-          className="ko:text-logo-check"
-          d="M65.9601 1.38557e-05L77.9054 11.964L37.9138 52.0168L25.9684 63.9807L0 37.9722L11.9454 26.0083L25.9684 40.053L65.9601 1.38557e-05Z"
-        />
-        <path fill="currentColor" className="ko:text-logo-slash" d="M126.768 0L74.8308 52.0167L86.7762 63.9807L138.713 11.964L126.768 0Z" />
-        <path
-          fill="currentColor"
-          className="ko:text-logo-cross"
-          d="M207.144 0.000134698L219.089 11.9641L199.093 31.9906L219.089 52.0171L207.143 63.981L187.148 43.9544L167.152 63.9809L155.207 52.0169L175.202 31.9905L155.207 11.9639L167.152 1.38557e-05L187.148 20.0267L207.144 0.000134698Z"
-        />
-        <path
-          fill="currentColor"
-          className="ko:text-logo-percent-numerator"
-          d="M255.196 9.56323C255.196 14.8449 250.921 19.1265 245.647 19.1265C240.374 19.1265 236.099 14.8449 236.099 9.56323C236.099 4.28161 240.374 1.38557e-05 245.647 1.38557e-05C250.921 1.38557e-05 255.196 4.28161 255.196 9.56323Z"
-        />
-        <path fill="currentColor" className="ko:text-logo-percent-slash" d="M288.036 1.38557e-05L236.099 52.0167L248.044 63.9807L299.981 11.964L288.036 1.38557e-05Z" />
-        <path
-          fill="currentColor"
-          className="ko:text-logo-percent-denominator"
-          d="M300 54.4368C300 59.7184 295.725 64 290.452 64C285.178 64 280.903 59.7184 280.903 54.4368C280.903 49.1552 285.178 44.8736 290.452 44.8736C295.725 44.8736 300 49.1552 300 54.4368Z"
-        />
-      </svg>
-
-      {text && <div className="ko:font-display ko:font-bold ko:uppercase ko:tracking-[10%] ko:text-[2.25rem] ko:leading-[normal] ko:text-nowrap">{title}</div>}
+    <div className={twMerge(LogoVariants({ size }))}>
+      <LogoSvg titleId={titleId} title={title} text={text} className={twMerge("ko:shrink-0")} />
+      {text && <div>{title}</div>}
     </div>
   );
 }
