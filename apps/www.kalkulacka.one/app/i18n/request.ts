22aacde9d5d58b698cf6bdedc7144f0854a5856a
Kryštof Korb <krystof@korb.cz>
2025-08-08 17:10:21 +0200

    Clean up next-intl setup

--- apps/www.kalkulacka.one/app/i18n/request.ts
+++ apps/www.kalkulacka.one/app/i18n/request.ts
@@ -1,9 +1,9 @@
 import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
+
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  // Typically corresponds to the `[locale]` segment
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
 
