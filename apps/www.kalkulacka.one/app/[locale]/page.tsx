c321e0f7587e66f64ed4291d40341a9ffa6d3750
Kryštof Korb <krystof@korb.cz>
2025-08-08 17:30:27 +0200

    Use dynamic imports

--- apps/www.kalkulacka.one/app/[locale]/page.tsx
+++ apps/www.kalkulacka.one/app/[locale]/page.tsx
@@ -1,17 +1,14 @@
 import { notFound } from "next/navigation";
 
 import type { I18nParams } from "../i18n/params";
-import Czech from "./page.cs";
-import English from "./page.en";
 
 export default async function Page({ params }: { params: I18nParams }) {
   const { locale } = await params;
-  switch (locale) {
-    case "en":
-      return <English />;
-    case "cs":
-      return <Czech />;
-    default:
-      notFound();
+
+  try {
+    const Component = (await import(`./page.${locale}`)).default;
+    return <Component />;
+  } catch (error) {
+    notFound();
   }
 }
