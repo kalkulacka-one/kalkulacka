55b5ec88f480670d02f38cd27bbb871635b03249
Kryštof Korb <krystof@korb.cz>
2025-08-23 14:43:20 +0200

    Adjust layout hierarchy

---
+++ apps/www.volebnikalkulacka.cz/app/(web)/(kalkulacka)/kalkulacka/[calculator]/layout.tsx
@@ -0,0 +1,17 @@
+import "@/app/globals.css";
+
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ calculator: string }>;
+}) {
+  const { calculator } = await params;
+  return (
+    <section>
+      <span>Calculator: `{calculator}`</span>
+      <main>{children}</main>
+    </section>
+  );
+}
