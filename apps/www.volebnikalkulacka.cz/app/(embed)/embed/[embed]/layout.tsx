3ca3d868c23863364224f8f091b16b222e3c289a
Kryštof Korb <krystof@korb.cz>
2025-08-23 14:34:53 +0200

    Add import alias

--- apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/layout.tsx
+++ apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/layout.tsx
@@ -1,4 +1,4 @@
-import "./../../../globals.css";
+import "@/app/globals.css";
 
 export default async function RootLayout({
   children,
--- apps/www.volebnikalkulacka.cz/app/(web)/layout.tsx
+++ apps/www.volebnikalkulacka.cz/app/(web)/layout.tsx
@@ -1,5 +1,6 @@
-import "./../globals.css";
 import { Logo } from "@repo/design-system/client";
+
+import "@/app/globals.css";
 
 export default async function RootLayout({
   children,
