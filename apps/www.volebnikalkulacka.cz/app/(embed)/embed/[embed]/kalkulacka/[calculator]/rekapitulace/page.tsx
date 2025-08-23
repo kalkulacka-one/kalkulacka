2f8e7b1a1e0ffbdbe6f209c6dd3c4db6fb66fdd0
Michał Wierzgoń <michal.wierzgon@gmail.com>
2025-08-23 14:58:19 +0200

    Add routing for Czech app (#178)

---
+++ apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/kalkulacka/[calculator]/rekapitulace/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h2>Rekapitulace</h2>;
+}
---
+++ apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/layout.tsx
@@ -0,0 +1,21 @@
+import "../../../globals.css";
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ embed: string }>;
+}) {
+  const { embed } = await params;
+  return (
+    <html lang="cs">
+      <body>
+        <div>
+          <span>Embed: `{embed}`</span>
+          <main>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
