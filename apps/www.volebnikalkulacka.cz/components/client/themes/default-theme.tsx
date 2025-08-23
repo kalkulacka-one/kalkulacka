ad3c1bb2b418f2880174ca830f35bbd224f1a7f7
Kryštof Korb <krystof@korb.cz>
2025-08-23 15:59:45 +0200

    Add default theme component

---
+++ apps/www.volebnikalkulacka.cz/components/client/themes/default-theme.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import "@repo/design-system/themes/www.volebnikalkulacka.cz/default";
+
+export const DefaultTheme = ({ children }: { children: React.ReactNode }) => {
+  return children;
+};
