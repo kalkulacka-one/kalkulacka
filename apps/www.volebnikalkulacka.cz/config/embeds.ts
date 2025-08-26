8c9c6fd361fc1cbc96e518b53135e6ea18b68828
Kryštof Korb <krystof@korb.cz>
2025-08-26 16:58:15 +0200

    Add embeds config

---
+++ apps/www.volebnikalkulacka.cz/config/embeds.ts
@@ -0,0 +1,15 @@
+import type { ThemeName } from "./themes";
+
+export type EmbedConfig = {
+  theme: ThemeName;
+};
+
+export const embedsConfig = {
+  default: { theme: "default" },
+} as const satisfies Record<string, EmbedConfig>;
+
+export type EmbedName = keyof typeof embedsConfig;
+
+export function isEmbedName(name: string): name is EmbedName {
+  return Object.prototype.hasOwnProperty.call(embedsConfig, name);
+}
