5d4bb073b491eb2200c8e2aa31faa50548960662
Michał Wierzgoń <michal.wierzgon@gmail.com>
2025-08-26 20:00:14 +0200

    Add internationalization support to Kalkulacka.1 app (#151)

---
+++ apps/www.kalkulacka.one/app/[locale]/page.tsx
@@ -0,0 +1,14 @@
+import { notFound } from "next/navigation";
+
+import type { I18nParams } from "../i18n/params";
+
+export default async function Page({ params }: { params: I18nParams }) {
+  const { locale } = await params;
+
+  try {
+    const Component = (await import(`./page.${locale}`)).default;
+    return <Component />;
+  } catch (error) {
+    notFound();
+  }
+}
