7735fc8e45a65932f623b109d1313101d6119fb9
Kryštof Korb <krystof@korb.cz>
2025-08-27 15:50:27 +0200

    Use commit SHA as revision

--- apps/www.volebnikalkulacka.cz/instrumentation.ts
+++ apps/www.volebnikalkulacka.cz/instrumentation.ts
@@ -3,6 +3,7 @@ import { registerOTel } from "@vercel/otel";
 const name = process.env.APPSIGNAL_NAME;
 const environment = process.env.APPSIGNAL_ENVIRONMENT;
 const pushApiKey = process.env.APPSIGNAL_PUSH_API_KEY;
+const revision = process.env.VERCEL_GIT_COMMIT_SHA;
 
 export function register() {
   registerOTel({
@@ -10,7 +11,7 @@ export function register() {
     attributes: {
       "appsignal.config.name": name,
       "appsignal.config.environment": environment,
-      "appsignal.config.revision": "1",
+      "appsignal.config.revision": revision,
       "appsignal.config.language_integration": "nodejs",
       "appsignal.config.push_api_key": pushApiKey,
       "host.name": "vercel",
