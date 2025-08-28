551d6185ca869967806b808ea0e9bb11481c9f9b
mewdev <michal.wierzgon@gmail.com>
2025-08-28 14:23:12 +0200

    Refactor SteppedProgressBar and add test

--- apps/design-system.kalkulacka.one/stories/SteppedProgressBar.stories.ts
+++ apps/design-system.kalkulacka.one/stories/SteppedProgressBar.stories.ts
@@ -2,18 +2,29 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { SteppedProgressBar } from "@repo/design-system/server";
 
-const questions = [
-  { id: 1, answer: 1 },
-  { id: 2, answer: 2 },
-  { id: 2, answer: 3 },
-  { id: 2, answer: 3 },
+const stepItems = [
+  { id: "1", status: null },
+  { id: "2", status: false },
+  { id: "3", status: true },
+  { id: "4", status: null },
+  { id: "5", status: undefined },
 ];
 
-const meta: Meta<typeof SteppedProgressBar> = {
+type ItemType = (typeof stepItems)[0];
+
+const meta: Meta<typeof SteppedProgressBar<ItemType>> = {
   title: "Components/SteppedProgressBar",
   component: SteppedProgressBar,
   tags: ["autodocs"],
   argTypes: {
+    idKey: {
+      control: "select",
+      options: ["id", "status"],
+    },
+    statusKey: {
+      control: "select",
+      options: ["id", "status"],
+    },
     stepCurrent: {
       control: {
         type: "number",
@@ -21,22 +32,20 @@ const meta: Meta<typeof SteppedProgressBar> = {
       },
     },
     stepTotal: {
-      control: {
-        type: "number",
-        min: 2,
-        max: 25,
-      },
+      control: false,
     },
   },
 };
 
-type SteppedProgressBarStory = StoryObj<typeof SteppedProgressBar>;
+type SteppedProgressBarStory = StoryObj<typeof meta>;
 
 export const Default: SteppedProgressBarStory = {
   args: {
-    questions: questions,
-    stepCurrent: 1,
-    stepTotal: questions.length,
+    stepItems: stepItems,
+    stepCurrent: 4,
+    stepTotal: stepItems.length,
+    idKey: "id",
+    statusKey: "status",
   },
 };
 
