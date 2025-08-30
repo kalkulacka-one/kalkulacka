3dc7e9295d8e67545e1bf07df2b679d95f698cbf
Kryštof Korb <krystof@korb.cz>
2025-08-30 19:14:41 +0200

    Update stories locations

--- apps/www.volebnikalkulacka.cz/.storybook/main.ts
+++ apps/www.volebnikalkulacka.cz/.storybook/main.ts
@@ -9,7 +9,7 @@ function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../components/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../calculator/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [],
   framework: {
     name: getAbsolutePath("@storybook/nextjs"),
