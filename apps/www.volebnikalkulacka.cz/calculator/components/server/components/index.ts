74318a66abd1f6deea1bbce5c6bbdc0d4f862300
mewdev <michal.wierzgon@gmail.com>
2025-09-02 17:00:56 +0200

    Add GuideComponent and update GuidePage to include it

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/index.ts
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/index.ts
@@ -1,1 +1,2 @@
+export * from "./guideComponent";
 export * from "./introduction";
--- apps/www.volebnikalkulacka.cz/calculator/components/server/pages/guide.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/pages/guide.tsx
@@ -1,5 +1,6 @@
 import type { Calculator } from "../../../../../../packages/schema/schemas/calculator.schema";
 import { Introduction } from "../components";
+import { GuideComponent } from "../components/guideComponent";
 
 export type GuidePage = {
   calculator: Calculator;
@@ -10,5 +11,7 @@ export function GuidePage({ calculator, step }: GuidePage) {
   switch (step) {
     case 1:
       return <Introduction calculator={calculator} />;
+    case 2:
+      return <GuideComponent href="/" />;
   }
 }
