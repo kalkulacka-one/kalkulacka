70bff75bf39e2d6af34bb4e17b250c870f7c286a
Kryštof Korb <krystof@korb.cz>
2025-09-02 21:17:54 +0200

    Add store & view model concept to app (#221)

---
+++ apps/www.volebnikalkulacka.cz/calculator/stores/calculator.tsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+import { type StoreApi, useStore } from "zustand";
+
+import type { CalculatorData } from "../lib";
+
+type CalculatorStoreState = {
+  calculator: CalculatorData;
+};
+
+// biome-ignore lint/complexity/noBannedTypes: To be defined
+type CalculatorStoreActions = {};
+
+export type CalculatorStore = CalculatorStoreState & CalculatorStoreActions;
+
+export const CalculatorStoreContext = createContext<StoreApi<CalculatorStore> | undefined>(undefined);
+
+export function useCalculatorStore<U>(selector: (state: CalculatorStore) => U): U {
+  const store = useContext(CalculatorStoreContext);
+  if (!store) {
+    throw new Error("Missing CalculatorStoreProvider");
+  }
+  return useStore(store, selector);
+}
---
+++ apps/www.volebnikalkulacka.cz/calculator/view-models/calculator.ts
@@ -0,0 +1,9 @@
+import type { Calculator } from "../../../../packages/schema/schemas/calculator.schema";
+import { useCalculatorStore } from "../stores";
+
+export type CalculatorViewModel = Calculator;
+
+export function useCalculatorViewModel(): CalculatorViewModel {
+  const calculator = useCalculatorStore((state) => state.calculator.calculator);
+  return calculator;
+}
