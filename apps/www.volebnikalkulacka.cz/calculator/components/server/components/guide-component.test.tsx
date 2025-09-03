7c49b73116a3c3e39d47a2a00674f0d5a0183788
Kryštof Korb <krystof@korb.cz>
2025-09-03 15:23:24 +0200

    Simplify test

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/guide-component.test.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/guide-component.test.tsx
@@ -4,15 +4,10 @@ import { describe, expect, it } from "vitest";
 import { GuideComponent } from ".";
 
 describe("GuideComponent", () => {
-  it("renders guide text", () => {
+  it("renders content", () => {
     render(<GuideComponent />);
 
     expect(screen.getByText(/Odpovídejte ANO\/NE nebo otázku přeskočte/)).toBeInTheDocument();
-  });
-
-  it("renders scoring explanation cards", () => {
-    render(<GuideComponent />);
-
     expect(screen.getByText("✔✔ Při shodě s Vaší odpovědí strana nebo politik dostane bod")).toBeInTheDocument();
     expect(screen.getByText("✔✘ Při neshodě bod naopak ztratí")).toBeInTheDocument();
     expect(screen.getByText("⭐ Pro vás důležitá témata označte hvězdičkou. Odpověď pak bude mít ve výpočtu shody dvojnásobnou váhu.")).toBeInTheDocument();
