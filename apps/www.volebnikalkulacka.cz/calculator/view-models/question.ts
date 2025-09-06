417cfa972f466bdc82cb1373c58ccfa4691e6e3c
Kryštof Korb <krystof@korb.cz>
2025-09-06 12:09:14 +0200

    Split question view model from questions (#242)

---
+++ apps/www.volebnikalkulacka.cz/calculator/view-models/question.ts
@@ -0,0 +1,3 @@
+import type { Question } from "../../../../packages/schema/schemas/question.schema";
+
+export type QuestionViewModel = Question;
--- apps/www.volebnikalkulacka.cz/calculator/view-models/questions.ts
+++ apps/www.volebnikalkulacka.cz/calculator/view-models/questions.ts
@@ -1,9 +1,12 @@
-import type { Question } from "../../../../packages/schema/schemas/question.schema";
 import { useCalculatorStore } from "../stores";
+import type { QuestionViewModel } from "./question";
 
-export type QuestionViewModel = Question;
+export type QuestionsViewModel = {
+  questions: QuestionViewModel[];
+  total: number;
+};
 
-export function useQuestionsViewModel(): { questions: Question[]; total: number } {
+export function useQuestionsViewModel(): QuestionsViewModel {
   const questions = useCalculatorStore((state) => state.calculator.questions);
   return {
     questions,
