22f6d1e0e3008cffad95094aa20aeb8a995a745b
Kryštof Korb <krystof@korb.cz>
2025-09-06 12:26:14 +0200

    Update Next.js pages

--- apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/(one-segment)/[first]/rekapitulace/page.tsx
+++ apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/(one-segment)/[first]/rekapitulace/page.tsx
@@ -1,3 +1,7 @@
-import Page from "../../../../../../(web)/(kalkulacka)/(one-segment)/[first]/rekapitulace/page";
+import { ReviewPageWithRouting } from "../../../../../../../components/client";
 
-export default Page;
+export default async function Page({ params }: { params: Promise<{ embed: string; first: string }> }) {
+  const { embed, first } = await params;
+
+  return <ReviewPageWithRouting segments={{ first, embed }} />;
+}
--- apps/www.volebnikalkulacka.cz/app/(web)/(kalkulacka)/(two-segments)/[first]/[second]/rekapitulace/page.tsx
+++ apps/www.volebnikalkulacka.cz/app/(web)/(kalkulacka)/(two-segments)/[first]/[second]/rekapitulace/page.tsx
@@ -1,3 +1,7 @@
-import Page from "../../../../(one-segment)/[first]/rekapitulace/page";
+import { ReviewPageWithRouting } from "../../../../../../../components/client";
 
-export default Page;
+export default async function Page({ params }: { params: Promise<{ first: string; second: string }> }) {
+  const { first, second } = await params;
+
+  return <ReviewPageWithRouting segments={{ first, second }} />;
+}
