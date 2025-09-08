97dd3d459ace4e16961bbf45443cc666eb3e10f9
klarasch <klara.scholleova@gmail.com>
2025-09-08 06:30:59 +0200

    card adjustments

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/introduction.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/introduction.tsx
@@ -1,3 +1,5 @@
+import { Card } from "@repo/design-system/server";
+
 import type { CalculatorViewModel } from "../../../view-models";
 
 export type Introduction = {
@@ -8,8 +10,8 @@ export function Introduction({ calculator }: Introduction) {
   const { title, intro } = calculator;
   return (
     <>
-      <h1>{title}</h1>
-      <p>{intro}</p>
+      <h1 className="text-2xl ko:font-display font-semibold mb-2 tracking-[-.03em]">{title}</h1>
+      <p className="tracking-[0.03em]">{intro}</p>
     </>
   );
 }
--- packages/design-system/src/components/server/card.tsx
+++ packages/design-system/src/components/server/card.tsx
@@ -5,7 +5,7 @@ export type Card = {
   children: React.ReactNode;
 } & VariantProps<typeof CardVariants>;
 
-const CardVariants = cva("ko:rounded-lg", {
+const CardVariants = cva("ko:rounded-3xl", {
   variants: {
     color: {
       white: "ko:bg-white",
@@ -17,7 +17,7 @@ const CardVariants = cva("ko:rounded-lg", {
       false: "",
     },
     shadow: {
-      true: "ko:shadow-md",
+      true: "ko:shadow-lg",
       false: "",
     },
   },
