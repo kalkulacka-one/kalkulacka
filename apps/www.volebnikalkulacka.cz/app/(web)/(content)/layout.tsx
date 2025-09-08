e55606018715132eda3961e3e07456f9dd538ede
Kryštof Korb <krystof@korb.cz>
2025-09-08 11:04:22 +0200

    Use header component in content layout

--- apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
+++ apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
@@ -1,16 +1,9 @@
+import { Header } from "../../../components/server";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <header>
-        <h1>Volební kalkulačka</h1>
-        <nav>
-          <a href="/">Domů</a>
-          <a href="/o-nas">O nás</a>
-          <a href="/metodika">Metodika</a>
-          <a href="/soukromi">Ochrana soukromí</a>
-          <a href="/kontakt">Kontakt</a>
-        </nav>
-      </header>
+      <Header />
       <main>{children}</main>
     </div>
   );
