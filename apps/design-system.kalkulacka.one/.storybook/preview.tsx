a9c0bffcb2ac55689f86e6473d2ab0a9bda0a875
Kryštof Korb <krystof@korb.cz>
2025-09-08 14:00:54 +0200

    Add Alarm embed and theme to Czech app

--- apps/design-system.kalkulacka.one/.storybook/preview.tsx
+++ apps/design-system.kalkulacka.one/.storybook/preview.tsx
@@ -10,6 +10,9 @@ const themeLoaders: Record<string, () => Promise<string>> = {
   "Volební kalkulačka (CZ)": async () =>
     // @ts-expect-error
     (await import("!css-loader!@repo/design-system/themes/www.volebnikalkulacka.cz/default")).default,
+  "Volební kalkulačka (CZ) — Alarm": async () =>
+    // @ts-expect-error
+    (await import("!css-loader!@repo/design-system/themes/www.volebnikalkulacka.cz/alarm")).default,
 };
 
 const themeNames = Object.keys(themeLoaders);
