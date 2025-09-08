934fb556271d6a1de5459f24d28afae70edac381
Michal Škop <michal.skop@kohovolit.eu>
2025-09-08 16:41:56 +0200

    Add candidate's organization to view model (#257)

--- apps/www.volebnikalkulacka.cz/calculator/view-models/candidate.ts
+++ apps/www.volebnikalkulacka.cz/calculator/view-models/candidate.ts
@@ -7,6 +7,7 @@ import { personViewModel } from "./person";
 
 export type CandidateViewModel = Omit<Candidate, "nestedCandidates"> & {
   displayName: string | undefined;
+  organization?: string | undefined;
   nestedCandidates?: CandidateViewModel[];
 };
 
@@ -32,17 +33,36 @@ function getCandidateDisplayName(
   return undefined;
 }
 
+function getCandidateOrganization(
+  candidate: Candidate,
+  personsMap: Map<string, ReturnType<typeof personViewModel>>,
+  organizationsMap: Map<string, ReturnType<typeof organizationViewModel>>,
+): string | undefined {
+  const firstReference = candidate.references?.[0];
+  if (firstReference?.type === "person") {
+    const person = personsMap.get(firstReference.id);
+    if (person?.memberOf && person.memberOf.length > 0) {
+      const firstOrganizationId = person.memberOf[0]?.id;
+      return firstOrganizationId ? organizationsMap.get(firstOrganizationId)?.displayName : undefined;
+    }
+  }
+
+  return undefined;
+}
+
 export function candidateViewModel(
   candidate: Candidate,
   personsMap: Map<string, ReturnType<typeof personViewModel>>,
   organizationsMap: Map<string, ReturnType<typeof organizationViewModel>>,
 ): CandidateViewModel {
   const displayName = getCandidateDisplayName(candidate, personsMap, organizationsMap);
+  const organization = getCandidateOrganization(candidate, personsMap, organizationsMap);
   const nestedCandidates = candidate.nestedCandidates?.map((nested) => candidateViewModel(nested, personsMap, organizationsMap));
 
   return {
     ...candidate,
     displayName,
+    organization,
     nestedCandidates,
   };
 }
