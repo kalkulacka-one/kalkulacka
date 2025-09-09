05d4028ddc3016da6eef16104aa16a55f1c01354
Kryštof Korb <krystof@korb.cz>
2025-09-09 22:30:23 +0200

    Add introduction routes

---
+++ apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/(two-segments)/[first]/[second]/uvod/page.tsx
@@ -0,0 +1,7 @@
+import { IntroductionPageWithRouting } from "../../../../../../../../components/client";
+
+export default async function Page({ params }: { params: Promise<{ first: string; second: string; embed: string }> }) {
+  const { first, second, embed } = await params;
+
+  return <IntroductionPageWithRouting segments={{ first, second, embed }} />;
+}
