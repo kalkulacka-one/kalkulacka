7906df90301f24d17d9a657219ff7e6734f7df70
Kryštof Korb <krystof@korb.cz>
2025-09-09 23:04:17 +0200

    Implement app header in result

--- apps/www.volebnikalkulacka.cz/calculator/components/server/pages/result.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/pages/result.tsx
@@ -1,14 +1,44 @@
-import type { ResultViewModel } from "../../../view-models";
-import { MatchCard } from "../components";
+import { mdiArrowLeft, mdiClose } from "@mdi/js";
+import { Button, Icon } from "@repo/design-system/client";
+
+import type { CalculatorViewModel, ResultViewModel } from "../../../view-models";
+import { WithCondenseOnScroll } from "../../client/app-header-with-scroll";
+import { AppHeader, AppHeaderBottom, AppHeaderBottomLeft, AppHeaderBottomMain, AppHeaderMain, AppHeaderRight, LayoutHeader, MatchCard } from "../components";
 
 export type ResultPage = {
   result: ResultViewModel;
+  calculator: CalculatorViewModel;
   onPreviousClick: () => void;
+  onCloseClick: () => void;
 };
 
-export function ResultPage({ result, onPreviousClick }: ResultPage) {
+export function ResultPage({ result, calculator, onPreviousClick, onCloseClick }: ResultPage) {
   return (
     <>
+      <LayoutHeader>
+        <WithCondenseOnScroll>
+          {(condensed) => (
+            <AppHeader condensed={condensed}>
+              <AppHeaderMain title="Volební kalkulačka" secondaryTitle={calculator?.title} tertiaryTitle="Sněmovní volby 2025" />
+              <AppHeaderRight>
+                <Button variant="link" color="neutral" size="small" aria-label="Close" onClick={onCloseClick}>
+                  <Icon icon={mdiClose} size="medium" decorative />
+                </Button>
+              </AppHeaderRight>
+              <AppHeaderBottom>
+                <AppHeaderBottomLeft condensed={condensed}>
+                  <Button variant="link" color="neutral" size="small" onClick={onPreviousClick} aria-label="Back">
+                    <Icon icon={mdiArrowLeft} size="medium" decorative />
+                  </Button>
+                </AppHeaderBottomLeft>
+                <AppHeaderBottomMain condensed={condensed}>
+                  <h3 className="ko:font-display font-semibold text-3xl">Výsledek</h3>
+                </AppHeaderBottomMain>
+              </AppHeaderBottom>
+            </AppHeader>
+          )}
+        </WithCondenseOnScroll>
+      </LayoutHeader>
       {result.matches.map((match) => (
         <MatchCard key={match.candidate.id} candidate={match.candidate} order={match.order} match={match.match} />
       ))}
