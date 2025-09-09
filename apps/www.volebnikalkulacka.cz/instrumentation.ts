bf0f81157b63fe262239c2a3925e4de30c07b03e
Kryštof Korb <krystof@korb.cz>
2025-09-09 14:22:35 +0200

    Add fallback for commit SHA

--- apps/www.volebnikalkulacka.cz/instrumentation.ts
+++ apps/www.volebnikalkulacka.cz/instrumentation.ts
@@ -3,7 +3,7 @@ import { registerOTel } from "@vercel/otel";
 const name = process.env.APPSIGNAL_NAME;
 const environment = process.env.APPSIGNAL_ENVIRONMENT;
 const pushApiKey = process.env.APPSIGNAL_PUSH_API_KEY;
-const revision = process.env.VERCEL_GIT_COMMIT_SHA;
+const revision = process.env.VERCEL_GIT_COMMIT_SHA && process.env.VERCEL_GIT_COMMIT_SHA.trim() !== "" ? process.env.VERCEL_GIT_COMMIT_SHA : "development";
 
 export function register() {
   registerOTel({
