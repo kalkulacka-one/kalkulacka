a684937d43f387cf6db79458ec0740a64f02f915
Kryštof Korb <krystof@korb.cz>
2025-09-10 00:14:27 +0200

    Add spacer to bottom layout navigation

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/layout.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/layout.tsx
@@ -14,6 +14,17 @@ export function LayoutContent({ children }: { children: React.ReactNode }) {
   return <main className="px-4 py-6">{children}</main>;
 }
 
-export function LayoutBottomNavigation({ children }: { children: React.ReactNode }) {
-  return <div className="fixed bottom-0 left-0 right-0">{children}</div>;
+export function LayoutBottomNavigation({ children, spacer = "5rem" }: { children: React.ReactNode; spacer?: string | false }) {
+  return (
+    <>
+      <div className="fixed bottom-0 left-0 right-0">{children}</div>
+      {spacer && (
+        <style jsx global>{`
+          body {
+            padding-bottom: ${spacer};
+          }
+        `}</style>
+      )}
+    </>
+  );
 }
