4ba1f7b583735ab64a75b4339a9a9c3a5c9a9590
Kryštof Korb <krystof@korb.cz>
2025-09-10 15:45:23 +0200

    Add legacy redirects to Czech site (#283)

--- apps/www.volebnikalkulacka.cz/next.config.js
+++ apps/www.volebnikalkulacka.cz/next.config.js
@@ -13,4 +13,44 @@ module.exports = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/metodika-tvorby-otazek",
+        destination: "/metodika",
+        permanent: true,
+      },
+      {
+        source: "/ochrana-dat",
+        destination: "/soukromi",
+        permanent: true,
+      },
+      // 2024 archive redirects
+      {
+        source: "/o-volbach",
+        destination: "https://archiv-2024.volebnikalkulacka.cz/o-volbach",
+        permanent: false,
+      },
+      {
+        source: "/archiv",
+        destination: "https://archiv-2024.volebnikalkulacka.cz/archiv",
+        permanent: false,
+      },
+      {
+        source: "/volby/krajske-2024/:path*",
+        destination: "https://archiv-2024.volebnikalkulacka.cz/volby/krajske-2024/:path*",
+        permanent: false,
+      },
+      {
+        source: "/volby/senatni-2024/:path*",
+        destination: "https://archiv-2024.volebnikalkulacka.cz/volby/senatni-2024/:path*",
+        permanent: false,
+      },
+      {
+        source: "/volby/evropske-2024/:path*",
+        destination: "https://archiv-2024.volebnikalkulacka.cz/volby/evropske-2024/:path*",
+        permanent: false,
+      },
+    ];
+  },
 };
