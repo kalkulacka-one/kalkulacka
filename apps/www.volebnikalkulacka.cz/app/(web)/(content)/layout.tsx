e97b6fbd35732fd840b1394cc3181823f5dd54df
Kryštof Korb <krystof@korb.cz>
2025-09-10 16:03:53 +0200

    Delete server header

--- apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
+++ apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
@@ -1,9 +1,6 @@
-import { Header } from "../../../components/server";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <Header />
       <main>{children}</main>
     </div>
   );
