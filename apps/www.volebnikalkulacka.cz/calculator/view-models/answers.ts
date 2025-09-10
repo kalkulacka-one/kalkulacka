cab5821a678ec40477cea97311d169635042d0ba
mewdev <michal.wierzgon@gmail.com>
2025-09-10 08:58:43 +0200

    Refactor QuestionProgressBar to use answersMap

--- apps/www.volebnikalkulacka.cz/calculator/view-models/answers.ts
+++ apps/www.volebnikalkulacka.cz/calculator/view-models/answers.ts
@@ -6,13 +6,20 @@ import { type AnswerViewModel, answerViewModel } from "./answer";
 
 export type AnswersViewModel = {
   answers: AnswerViewModel[];
+  answersMap: Map<string, boolean | null | undefined>;
   setAnswer: AnswersStore["setAnswer"];
   clearAnswers: AnswersStore["clearAnswers"];
 };
 
-export function answersViewModel(answers: Answer[], setAnswer: AnswersStore["setAnswer"], clearAnswers: AnswersStore["clearAnswers"]): AnswersViewModel {
+export function answersViewModel(
+  answers: Answer[],
+  setAnswer: AnswersStore["setAnswer"],
+  clearAnswers: AnswersStore["clearAnswers"],
+  answersMap: Map<string, boolean | null | undefined>,
+): AnswersViewModel {
   return {
     answers: answers.map((answer) => answerViewModel(answer, setAnswer)),
+    answersMap,
     setAnswer,
     clearAnswers,
   };
@@ -23,5 +30,7 @@ export function useAnswersViewModel(): AnswersViewModel {
   const setAnswer = useAnswersStore((state) => state.setAnswer);
   const clearAnswers = useAnswersStore((state) => state.clearAnswers);
 
-  return useMemo(() => answersViewModel(answers, setAnswer, clearAnswers), [answers, setAnswer, clearAnswers]);
+  const answersMap = useMemo(() => new Map(answers.map((a) => [a.questionId, a.answer])), [answers]);
+
+  return useMemo(() => answersViewModel(answers, setAnswer, clearAnswers, answersMap), [answers, setAnswer, clearAnswers, answersMap]);
 }
