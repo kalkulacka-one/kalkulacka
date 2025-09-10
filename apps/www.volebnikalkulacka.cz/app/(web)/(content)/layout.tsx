fc500f38aa1d91531237641c6cdc9ab84f7b63ab
Kryštof Korb <krystof@korb.cz>
2025-09-10 16:08:26 +0200

    Add new app header-based header

--- apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
+++ apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
@@ -1,6 +1,9 @@
+import { Header } from "../../../components/client";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <Header />
       <main>{children}</main>
     </div>
   );
--- apps/www.volebnikalkulacka.cz/components/client/index.ts
+++ apps/www.volebnikalkulacka.cz/components/client/index.ts
@@ -1,6 +1,7 @@
 "use client";
 
 export * from "./embed-provider";
+export * from "./header";
 export * from "./pages";
 export * from "./provider-layout";
 export * from "./theme-provider";
