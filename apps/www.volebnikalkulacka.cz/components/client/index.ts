c8d9d68808f267aa33c18746dfe8d738a0f3888d
Kryštof Korb <krystof@korb.cz>
2025-09-11 01:49:02 +0200

    Add hide on embed wrapper

--- apps/www.volebnikalkulacka.cz/components/client/index.ts
+++ apps/www.volebnikalkulacka.cz/components/client/index.ts
@@ -4,6 +4,7 @@ export * from "./embed-context-provider";
 export * from "./embed-only";
 export * from "./embed-provider";
 export * from "./header";
+export * from "./hide-on-embed";
 export * from "./pages";
 export * from "./provider-layout";
 export * from "./theme-provider";
