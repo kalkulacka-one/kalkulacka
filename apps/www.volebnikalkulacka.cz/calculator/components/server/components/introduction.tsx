a33ba96a8313e93be2a8a72c2806d13c3f0c9f77
Kryštof Korb <krystof@korb.cz>
2025-09-11 10:05:46 +0200

    Style links in Markdown in introduction

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/introduction.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/introduction.tsx
@@ -10,7 +10,17 @@ export function Introduction({ calculator }: Introduction) {
   const { intro } = calculator;
   return (
     <div className="grid gap-1 max-w-prose">
-      <Markdown allowedElements={["p", "strong", "em", "ul", "ol", "li", "a"]} skipHtml>
+      <Markdown
+        allowedElements={["p", "strong", "em", "ul", "ol", "li", "a"]}
+        skipHtml
+        components={{
+          a: ({ href, children }) => (
+            <a href={href} className="text-[var(--ko-color-primary)] text-[var(--ko-color-primary-hover)] underline" target="_blank" rel="noopener noreferrer">
+              {children}
+            </a>
+          ),
+        }}
+      >
         {intro}
       </Markdown>
     </div>
