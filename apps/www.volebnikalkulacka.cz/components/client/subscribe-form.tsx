4c05b7e810782856385fd3b71d6d2a71b9efb2a5
Kryštof Korb <krystof@korb.cz>
2025-09-11 17:17:08 +0200

    Fix form structure

--- apps/www.volebnikalkulacka.cz/components/client/subscribe-form.tsx
+++ apps/www.volebnikalkulacka.cz/components/client/subscribe-form.tsx
@@ -53,17 +53,21 @@ export function SubscribeForm() {
       ) : (
         <form className="flex flex-col gap-4 items-center" onSubmit={handleSubmit(onSubmit)} noValidate>
           <p className="text-center text-sm">Zjistěte, kdo se s vámi shodne na klíčových tématech. Nechte nám tu svůj e-mail a získejte přístup k nové kalkulačce mezi prvními.</p>
-          <div className="flex gap-4 justify-center">
-            <Field className="flex gap-4 relative" disabled={isSubmitting}>
-              <Label className="sr-only hidden">Zadejte váš email</Label>
-              <Input invalid={errors.email ? true : undefined} autoComplete="email" type="email" placeholder="E-mail" {...register("email")} />
-              {errors.email && <Description className="absolute -bottom-1.5 right-4 px-1 bg-white text-xs text-[var(--ko-palette-secondary)]">{errors.email.message}</Description>}
-            </Field>
-            <Button disabled={isSubmitting} type="submit" variant="outline" color="primary">
-              {isSubmitting ? "Odesílám" : "Odeslat"}
-            </Button>
-          </div>
-          {errors.root?.serverError && <Description className="text-sm text-center">⚠️ {errors.root?.serverError.message}</Description>}
+          <Field disabled={isSubmitting}>
+            <div className="grid grid-rows-2 gap-2 justify-center">
+              <div className="flex gap-4 justify-center">
+                <Label className="sr-only hidden">Zadejte váš email</Label>
+                <Input invalid={errors.email ? true : undefined} autoComplete="email" type="email" placeholder="E-mail" {...register("email")} />
+                <Button disabled={isSubmitting} type="submit" variant="outline" color="primary">
+                  {isSubmitting ? "Odesílám" : "Odeslat"}
+                </Button>
+              </div>
+              <div className="text-center">
+                {errors.email && <Description className="text-xs text-[var(--ko-palette-secondary)]">{errors.email.message}</Description>}
+                {errors.root?.serverError && <Description className="text-sm">⚠️ {errors.root?.serverError.message}</Description>}
+              </div>
+            </div>
+          </Field>
           <p className="text-sm leading-[1.23]">Odesláním souhlasíte se zasíláním novinek o volební kalkulačce.</p>
         </form>
       )}
