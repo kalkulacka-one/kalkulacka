ca1d37eead2f21bcf8194e774f3df9f6fa8cf69f
Kryštof Korb <krystof@korb.cz>
2025-09-11 10:08:21 +0200

    Fix lint

--- apps/www.volebnikalkulacka.cz/components/client/themes/index.ts
+++ apps/www.volebnikalkulacka.cz/components/client/themes/index.ts
@@ -1,3 +1,3 @@
+export { AlarmTheme } from "./alarm-theme";
 export { DefaultTheme } from "./default-theme";
 export { PrimaTheme } from "./prima-theme";
-export { AlarmTheme } from "./alarm-theme";
