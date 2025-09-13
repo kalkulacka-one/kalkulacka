1a98bbe83ce7fe5430780a364866ff192cc0c380
Kryštof Korb <krystof@korb.cz>
2025-09-13 14:38:23 +0200

    Remove attribution wrapper

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/navigation-card-attribution.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/navigation-card-attribution.tsx
@@ -4,17 +4,15 @@ import { canonical } from "../../../../lib/routing/url-builders";
 
 export function NavigationCardAttribution() {
   return (
-    <div className="flex justify-center">
-      <a href={canonical.homepage()} target="_blank" className="group p-2 flex items-center gap-2 rounded-lg text-sm text-slate-600 hover:text-slate-800 hover:bg-slate-100">
-        <span>Přináší</span>
-        <div className="group-hover:hidden">
-          <Logo title="Volební kalkulačka" size="small" monochrome />
-        </div>
-        <div className="hidden group-hover:block">
-          <Logo title="Volební kalkulačka" size="small" />
-        </div>
-        <span>Volební kalkulačka</span>
-      </a>
-    </div>
+    <a href={canonical.homepage()} target="_blank" className="group p-2 flex items-center gap-2 rounded-lg text-sm text-slate-600 hover:text-slate-800 hover:bg-slate-100">
+      <span>Přináší</span>
+      <div className="group-hover:hidden">
+        <Logo title="Volební kalkulačka" size="small" monochrome />
+      </div>
+      <div className="hidden group-hover:block">
+        <Logo title="Volební kalkulačka" size="small" />
+      </div>
+      <span>Volební kalkulačka</span>
+    </a>
   );
 }
