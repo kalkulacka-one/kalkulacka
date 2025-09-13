17492d8021377ae26cb2eba221a62da7a3f68655
Kryštof Korb <krystof@korb.cz>
2025-09-13 12:29:38 +0200

    Add attribution to navigation card

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/navigation-card.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/navigation-card.tsx
@@ -1,14 +1,20 @@
 import { Card } from "@repo/design-system/server";
 
+import { NavigationCardAttribution } from "./navigation-card-attribution";
+
 export type NavigationCard = {
   children: React.ReactNode;
+  attributionHref?: string;
 };
 
-export function NavigationCard({ children }: NavigationCard) {
+export function NavigationCard({ children, attributionHref }: NavigationCard) {
   return (
     <div className="grid justify-items-end m-4">
       <Card corner="bottomRight" shadow="elevated">
-        <div className="p-4">{children}</div>
+        <div className="p-4">
+          {children}
+          {attributionHref && <NavigationCardAttribution href={attributionHref} />}
+        </div>
       </Card>
     </div>
   );
