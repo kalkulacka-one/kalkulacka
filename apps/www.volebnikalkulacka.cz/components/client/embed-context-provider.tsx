3f3311a1b9627e2f3a94053fb479961489a1661e
Kryštof Korb <krystof@korb.cz>
2025-09-13 13:34:22 +0200

    Make embed config optional

--- apps/www.volebnikalkulacka.cz/components/client/embed-context-provider.tsx
+++ apps/www.volebnikalkulacka.cz/components/client/embed-context-provider.tsx
@@ -5,18 +5,17 @@ import type { EmbedConfig, EmbedName } from "../../config/embeds";
 type EmbedContextType = {
   isEmbed: boolean;
   name: EmbedName | null;
-  config?: EmbedConfig | null;
+  config?: EmbedConfig;
 };
 
 const EmbedContext = createContext<EmbedContextType>({
   isEmbed: false,
   name: null,
-  config: undefined,
 });
 
 export const useEmbed = () => useContext(EmbedContext);
 
-export const EmbedContextProvider = ({ isEmbed, name, config, children }: { isEmbed: boolean; name: EmbedName | null; config?: EmbedConfig | undefined; children: React.ReactNode }) => {
+export const EmbedContextProvider = ({ isEmbed, name, config, children }: { isEmbed: boolean; name: EmbedName | null; config?: EmbedConfig; children: React.ReactNode }) => {
   const embedValue: EmbedContextType = {
     isEmbed,
     name,
