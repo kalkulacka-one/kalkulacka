be147c8666e512d3baa9758afccd49129673b23d
Kryštof Korb <krystof@korb.cz>
2025-09-13 13:35:26 +0200

    Remove `theme: "default"` as we have fallback now

--- apps/www.volebnikalkulacka.cz/config/embeds.ts
+++ apps/www.volebnikalkulacka.cz/config/embeds.ts
@@ -1,21 +1,21 @@
 import type { ThemeName } from "./themes";
 
 export type EmbedConfig = {
-  theme: ThemeName;
   logo?: "monochrome";
+  theme?: ThemeName;
 };
 
 export const embedsConfig = {
-  default: { theme: "default" },
+  default: {},
   "diky-ze-muzem": { theme: "diky-ze-muzem" },
   alarm: { theme: "alarm", logo: "monochrome" },
   prima: { theme: "prima", logo: "monochrome" },
-  idnes: { theme: "default" },
-  nova: { theme: "default" },
-  e15: { theme: "default" },
-  reflex: { theme: "default" },
-  blesk: { theme: "default" },
-  denik: { theme: "default" },
+  idnes: {},
+  nova: {},
+  e15: {},
+  reflex: {},
+  blesk: {},
+  denik: {},
 } as const satisfies Record<string, EmbedConfig>;
 
 export type EmbedName = keyof typeof embedsConfig;
