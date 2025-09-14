3c9e48f7e1c114000aff35dfe83df4cd1a1b22aa
Kryštof Korb <krystof@korb.cz>
2025-09-14 11:23:34 +0200

    Hotfix question card

--- apps/www.volebnikalkulacka.cz/calculator/components/server/components/question-card.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/server/components/question-card.tsx
@@ -12,14 +12,14 @@ export function QuestionCard({ question, current, total }: QuestionCard) {
   const { title, detail, statement } = question;
   return (
     <Card corner="topLeft" shadow="hard">
-      <div className="grid gap-4 p-8">
+      <div className="grid gap-3 p-4 sm:gap-4 sm:p-6">
         <div className="grid grid-flow-col auto-cols-max gap-2 items-center text-sm">
           <span className="font-bold">
             {current}/{total}
           </span>
           <span className="font-light">{title}</span>
         </div>
-        <h3 className="font-display text-2xl font-bold leading-tight max-w-2xl break-words">{statement}</h3>
+        <h3 className="font-display text-2xl font-bold leading-tight break-words">{statement}</h3>
         {detail && <p className="text-sm text-gray-900 leading-relaxed sm:text-base max-w-prose break-words">{detail}</p>}
       </div>
     </Card>
