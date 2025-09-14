3414e2fad48ee158e83e301e449e82ba651b20ef
Kryštof Korb <krystof@korb.cz>
2025-09-14 16:08:20 +0200

    Only register if configuration is provided

--- apps/www.volebnikalkulacka.cz/instrumentation.ts
+++ apps/www.volebnikalkulacka.cz/instrumentation.ts
@@ -6,15 +6,17 @@ const pushApiKey = process.env.APPSIGNAL_PUSH_API_KEY;
 const revision = process.env.VERCEL_GIT_COMMIT_SHA && process.env.VERCEL_GIT_COMMIT_SHA.trim() !== "" ? process.env.VERCEL_GIT_COMMIT_SHA : "development";
 
 export function register() {
-  registerOTel({
-    serviceName: name,
-    attributes: {
-      "appsignal.config.name": name,
-      "appsignal.config.environment": environment,
-      "appsignal.config.revision": revision,
-      "appsignal.config.language_integration": "nodejs",
-      "appsignal.config.push_api_key": pushApiKey,
-      "host.name": "vercel",
-    },
-  });
+  if (name && environment && pushApiKey) {
+    registerOTel({
+      serviceName: name,
+      attributes: {
+        "appsignal.config.name": name,
+        "appsignal.config.environment": environment,
+        "appsignal.config.revision": revision,
+        "appsignal.config.language_integration": "nodejs",
+        "appsignal.config.push_api_key": pushApiKey,
+        "host.name": "vercel",
+      },
+    });
+  }
 }
