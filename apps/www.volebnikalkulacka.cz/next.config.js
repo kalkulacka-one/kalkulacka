c1a91c2c862b3e7958af0ba3620e8ef67b88584a
Kryštof Korb <krystof@korb.cz>
2025-09-16 00:57:02 +0200

    Add more redirects to Czech app (#359)

--- apps/www.volebnikalkulacka.cz/next.config.js
+++ apps/www.volebnikalkulacka.cz/next.config.js
@@ -16,6 +16,11 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: "/volby/snemovni-2025",
+        destination: "/volby/snemovni-2025/kalkulacka",
+        permanent: false,
+      },
+      {
         source: "/metodika-tvorby-otazek",
         destination: "/metodika",
         permanent: true,
@@ -49,6 +54,22 @@ module.exports = {
       {
         source: "/volby/evropske-2024/:path*",
         destination: "https://archiv-2024.volebnikalkulacka.cz/volby/evropske-2024/:path*",
+        permanent: false,
+      },
+      // 2022 archive redirects
+      {
+        source: "/volby/prezidentske-2023/:path*",
+        destination: "https://archiv.volebnikalkulacka.cz/volby/prezidentske-2023/:path*",
+        permanent: false,
+      },
+      {
+        source: "/volby/senatni-2022/:path*",
+        destination: "https://archiv.volebnikalkulacka.cz/volby/senatni-2022/:path*",
+        permanent: false,
+      },
+      {
+        source: "/volby/komunalni-2022/:path*",
+        destination: "https://archiv.volebnikalkulacka.cz/volby/komunalni-2022/:path*",
         permanent: false,
       },
     ];
