8f51e2c7e17d947dee75802b6fadb1710759545a
mewdev <michal.wierzgon@gmail.com>
2025-09-15 21:50:38 +0200

    Refactor calculator card

--- apps/www.volebnikalkulacka.cz/calculator/components/client/calculator-card.tsx
+++ apps/www.volebnikalkulacka.cz/calculator/components/client/calculator-card.tsx
@@ -1,7 +1,5 @@
 import { Card } from "@repo/design-system/server";
 
-import { Button } from "../../../../../packages/design-system/dist/components/client/button";
-
 type CalculatorCard = {
   children: React.ReactNode;
 };
@@ -18,37 +16,30 @@ type CalculatorCardDescription = {
   children: React.ReactNode;
 };
 
+type CalculatorCardCTA = {
+  children: React.ReactNode;
+};
+
 function CalculatorCardHeader({ children }: CalculatorCardHeader) {
   return <div className="flex gap-2">{children}</div>;
 }
 
 function CalculatorCardTitle({ children }: CalculatorCardTitle) {
-  return (
-    <div className="justify-self-start max-w-sm">
-      <h3 className="text-left text-slate-700 font-sans text-3xl font-semibold tracking-tighter leading-tight max-w-2xl break-words group-hover:text-primary transition-colors">{children}</h3>
-    </div>
-  );
+  return <div className="justify-self-start max-w-sm text-left">{children}</div>;
 }
 
 function CalculatorCardDescription({ children }: CalculatorCardDescription) {
-  return (
-    <div className="justify-self-start">
-      <p className=" text-left font-sans text-sm text-slate-600 leading-relaxed sm:text-base max-w-prose break-words">{children}</p>
-    </div>
-  );
+  return <div className="justify-self-start text-left">{children}</div>;
+}
+
+function CalculatorCardCTA({ children }: CalculatorCardCTA) {
+  return <div className="grid items-stretch">{children}</div>;
 }
 
 export function CalculatorCard({ children }: CalculatorCard) {
   return (
     <Card shadow="hard" corner="topLeft">
-      <div className="grid gap-4 px-8 py-7 h-full items-start">
-        {children}
-        <div className="grid items-stretch">
-          <Button variant="fill" color="neutral">
-            Spustit kalkulačku
-          </Button>
-        </div>
-      </div>
+      <div className="grid gap-4 px-8 py-7 h-full items-start">{children}</div>
     </Card>
   );
 }
@@ -56,5 +47,6 @@ export function CalculatorCard({ children }: CalculatorCard) {
 CalculatorCard.Header = CalculatorCardHeader;
 CalculatorCard.Title = CalculatorCardTitle;
 CalculatorCard.Description = CalculatorCardDescription;
+CalculatorCard.Cta = CalculatorCardCTA;
 
-export { CalculatorCardHeader, CalculatorCardTitle, CalculatorCardDescription };
+export { CalculatorCardHeader, CalculatorCardTitle, CalculatorCardDescription, CalculatorCardCTA };
