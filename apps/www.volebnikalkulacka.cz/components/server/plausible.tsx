3e654ffb6906bcb8f07bf9f2260089178a4d358e
Kryštof Korb <krystof@korb.cz>
2025-09-16 14:02:04 +0200

    Restructure the environment variables correctly in Czech app (#364)

--- apps/www.volebnikalkulacka.cz/components/server/plausible.tsx
+++ apps/www.volebnikalkulacka.cz/components/server/plausible.tsx
@@ -2,16 +2,16 @@ import Script from "next/script";
 
 export function PlausibleScript() {
   const isProduction = process.env.VERCEL_ENV === "production";
-  const analyticsEnabled = process.env.NEXT_PUBLIC_ENABLE_ANALYTICS === "true" || isProduction;
-  const domain = process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN;
-  const useProxy = process.env.NEXT_PUBLIC_ANALYTICS_PROXY === "true";
+  const analyticsEnabled = process.env.ENABLE_ANALYTICS === "true" || isProduction;
+  const domain = process.env.ANALYTICS_DOMAIN;
+  const useProxy = process.env.ANALYTICS_PROXY === "true";
 
   if (!analyticsEnabled) {
     return null;
   }
 
   if (!domain) {
-    throw new Error("Missing `NEXT_PUBLIC_ANALYTICS_DOMAIN` environment variable");
+    throw new Error("Missing `ANALYTICS_DOMAIN` environment variable");
   }
 
   const scriptSrc = useProxy ? "/js/script.tagged-events.outbound-links.js" : "https://plausible.io/js/script.tagged-events.outbound-links.js";
