452ed46f970ac6173091a72460672fac31902055
Kryštof Korb <krystof@korb.cz>
2025-09-16 09:56:53 +0200

    Add environment variable for rewrites

--- apps/www.volebnikalkulacka.cz/components/server/plausible.tsx
+++ apps/www.volebnikalkulacka.cz/components/server/plausible.tsx
@@ -4,6 +4,7 @@ export function PlausibleScript() {
   const isProduction = process.env.VERCEL_ENV === "production";
   const analyticsEnabled = process.env.NEXT_PUBLIC_ENABLE_ANALYTICS === "true" || isProduction;
   const domain = process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN;
+  const useProxy = process.env.NEXT_PUBLIC_ANALYTICS_PROXY === "true";
 
   if (!analyticsEnabled) {
     return null;
@@ -13,5 +14,7 @@ export function PlausibleScript() {
     throw new Error("Missing `NEXT_PUBLIC_ANALYTICS_DOMAIN` environment variable");
   }
 
-  return <Script defer data-domain={domain} src="/js/script.tagged-events.outbound-links.js" />;
+  const scriptSrc = useProxy ? "/js/script.tagged-events.outbound-links.js" : "https://plausible.io/js/script.tagged-events.outbound-links.js";
+
+  return <Script defer data-domain={domain} src={scriptSrc} />;
 }
