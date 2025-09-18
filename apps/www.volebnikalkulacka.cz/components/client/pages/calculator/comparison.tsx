d1f2645361c4bf6cf81cd480471f95b1dbd40bbc
mewdev <michal.wierzgon@gmail.com>
2025-09-18 11:07:00 +0200

    Add basic comparison grid

--- apps/www.volebnikalkulacka.cz/components/client/pages/calculator/comparison.tsx
+++ apps/www.volebnikalkulacka.cz/components/client/pages/calculator/comparison.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/navigation";
 
 import { ComparisonPage } from "../../../../calculator/components/server";
-import { useAnswers, useCalculator, useQuestions } from "../../../../calculator/view-models";
+import { useAnswers, useCalculator, useQuestions, useResult } from "../../../../calculator/view-models";
 import { type RouteSegments, routes } from "../../../../lib/routing/route-builders";
 import { useEmbed } from "../../embed-context-provider";
 
 export function ComparisonPageWithRouting({ segments }: { segments: RouteSegments }) {
   const router = useRouter();
   const calculator = useCalculator();
+  const result = useResult();
   const answers = useAnswers();
   const questions = useQuestions();
   const embed = useEmbed();
@@ -22,5 +23,7 @@ export function ComparisonPageWithRouting({ segments }: { segments: RouteSegment
 
   const attribution = embed.isEmbed && (embed.config?.navigationAttribution ?? true);
 
-  return <ComparisonPage calculator={calculator} answers={answers} questions={questions} onPreviousClick={handlePreviousClick} onCloseClick={handleCloseClick} attribution={attribution} />;
+  return (
+    <ComparisonPage calculator={calculator} result={result} answers={answers} questions={questions} onPreviousClick={handlePreviousClick} onCloseClick={handleCloseClick} attribution={attribution} />
+  );
 }
