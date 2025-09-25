39606a86a3aa3e5c05708182190326757a14b681
mewdev <michal.wierzgon@gmail.com>
2025-09-25 14:53:28 +0200

    Add draft rozcestnik

--- apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
+++ apps/www.volebnikalkulacka.cz/app/(web)/(content)/layout.tsx
@@ -3,7 +3,7 @@ import { Footer } from "../../../components/server";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-1">
+    <div className="grid grid-cols-1 grid-rows-[auto_1fr_auto] min-h-dvh">
       <Header />
       {children}
       <Footer />
