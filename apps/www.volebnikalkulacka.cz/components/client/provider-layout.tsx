d78552885656dbaede7d64d26e3f630297b30278
Kryštof Korb <krystof@korb.cz>
2025-09-30 07:41:25 +0200

    Split layout provider into two (#395)

--- apps/www.volebnikalkulacka.cz/components/client/provider-layout.tsx
+++ apps/www.volebnikalkulacka.cz/components/client/provider-layout.tsx
@@ -3,19 +3,15 @@ import type { PropsWithChildren } from "react";
 import { AnswersStoreProvider, CalculatorStoreProvider } from "../../calculator/components/client";
 import { Layout as AppLayout } from "../../calculator/components/server";
 import type { CalculatorData } from "../../calculator/lib";
-import { SessionDataLoader } from "./session-data-loader";
-import { SessionInitializer } from "./session-initializer";
 
-type ProviderLayoutProps = PropsWithChildren<{
+export type ProviderLayout = PropsWithChildren<{
   calculatorData: CalculatorData;
 }>;
 
-export function ProviderLayout({ calculatorData, children }: ProviderLayoutProps) {
+export function ProviderLayout({ calculatorData, children }: ProviderLayout) {
   return (
     <CalculatorStoreProvider calculatorData={calculatorData}>
       <AnswersStoreProvider>
-        <SessionInitializer />
-        <SessionDataLoader />
         <AppLayout>{children}</AppLayout>
       </AnswersStoreProvider>
     </CalculatorStoreProvider>
---
+++ apps/www.volebnikalkulacka.cz/components/client/session-provider-layout.tsx
@@ -0,0 +1,20 @@
+import type { PropsWithChildren } from "react";
+
+import type { CalculatorData } from "../../calculator/lib";
+import { ProviderLayout } from "./provider-layout";
+import { SessionDataLoader } from "./session-data-loader";
+import { SessionInitializer } from "./session-initializer";
+
+export type SessionProviderLayout = PropsWithChildren<{
+  calculatorData: CalculatorData;
+}>;
+
+export function SessionProviderLayout({ calculatorData, children }: SessionProviderLayout) {
+  return (
+    <ProviderLayout calculatorData={calculatorData}>
+      <SessionInitializer />
+      <SessionDataLoader />
+      {children}
+    </ProviderLayout>
+  );
+}
