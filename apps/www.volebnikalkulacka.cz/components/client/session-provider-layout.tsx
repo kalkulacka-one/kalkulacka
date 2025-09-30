a6368093d6a7ef49f40857f4034eb45c557acc1b
Kryštof Korb <krystof@korb.cz>
2025-09-30 07:37:50 +0200

    Fix types

--- apps/www.volebnikalkulacka.cz/components/client/session-provider-layout.tsx
+++ apps/www.volebnikalkulacka.cz/components/client/session-provider-layout.tsx
@@ -5,11 +5,11 @@ import { ProviderLayout } from "./provider-layout";
 import { SessionDataLoader } from "./session-data-loader";
 import { SessionInitializer } from "./session-initializer";
 
-type SessionProviderLayoutProps = PropsWithChildren<{
+export type SessionProviderLayout = PropsWithChildren<{
   calculatorData: CalculatorData;
 }>;
 
-export function SessionProviderLayout({ calculatorData, children }: SessionProviderLayoutProps) {
+export function SessionProviderLayout({ calculatorData, children }: SessionProviderLayout) {
   return (
     <ProviderLayout calculatorData={calculatorData}>
       <SessionInitializer />
