16edc2d70268bea9e15c72e5e542bfb0fa669642
mewdev <michal.wierzgon@gmail.com>
2025-10-21 09:44:45 +0200

    Delete zod schemas

--- packages/schema/schemas/elections.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-import { electionSchema } from "./election.schema";
-
-export const electionsSchema = z.array(electionSchema).describe("List of elections");
-
-export type Elections = z.infer<typeof electionsSchema>;
