c255fa6c31055d4ab3dadf532d57fb72f936f2a7
boa <boa@protonmail.com>
2025-10-30 14:17:23 +0100

    change socials

--- apps/www.voksmonitor.hu/components/server/footer.tsx
+++ apps/www.voksmonitor.hu/components/server/footer.tsx
@@ -1,4 +1,4 @@
-import { mdiInstagram } from "@mdi/js";
+import { mdiFacebook, mdiInstagram } from "@mdi/js";
 import { Icon, Logo } from "@repo/design-system/client";
 import Link from "next/link";
 
@@ -17,17 +17,15 @@ export function Footer() {
               </p>
               <div className="grid grid-cols-2 gap-2 mt-4 w-fit">
                 <Link
-                  href="https://www.instagram.com/volebnikalk"
+                  href="https://www.instagram.com/kmonitorhu/"
                   target="_blank"
                   className="text-slate-400 hover:text-white transition-colors flex items-center justify-center"
                   aria-label="Instagram"
                 >
                   <Icon icon={mdiInstagram} size="medium" decorative />
                 </Link>
-                <Link href="https://twitter.com/volebnikalk" target="_blank" className="text-slate-400 hover:text-white transition-colors flex items-center justify-center" aria-label="X (Twitter)">
-                  <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                    <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-                  </svg>
+                <Link href="https://www.facebook.com/Kmonitor/" target="_blank" className="text-slate-400 hover:text-white transition-colors flex items-center justify-center" aria-label="Facebook">
+                  <Icon icon={mdiFacebook} size="medium" decorative />
                 </Link>
               </div>
             </div>
