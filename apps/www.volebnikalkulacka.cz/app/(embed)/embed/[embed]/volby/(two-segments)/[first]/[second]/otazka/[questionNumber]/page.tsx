df4d10ce63d69d0f2a24e985623bcf3bb8a1cbdf
Kryštof Korb <krystof@korb.cz>
2025-11-08 12:12:38 +0100

    Add client side routing to calculator questions (#250)

--- apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/volby/(two-segments)/[first]/[second]/otazka/[questionNumber]/page.tsx
+++ apps/www.volebnikalkulacka.cz/app/(embed)/embed/[embed]/volby/(two-segments)/[first]/[second]/otazka/[questionNumber]/page.tsx
@@ -2,16 +2,13 @@ import type { Metadata } from "next";
 
 import { QuestionPageWithRouting } from "../../../../../../../../../../components/client";
 import { generateCalculatorMetadata } from "../../../../../../../../../../lib/metadata/calculator";
-import { questionNumberGuard, validateQuestionNumber } from "../../../../../../../../../../lib/routing/guards/question-number";
+import { questionNumberGuard } from "../../../../../../../../../../lib/routing/guards/question-number";
 import { canonical } from "../../../../../../../../../../lib/routing/url-builders";
 
 export async function generateMetadata({ params }: { params: Promise<{ first: string; second: string; questionNumber: string }> }): Promise<Metadata> {
   const { first, second, questionNumber } = await params;
-  const validatedNumber = validateQuestionNumber(questionNumber);
-  if (validatedNumber === null) {
-    throw new Error(`Invalid question number \`${questionNumber}\``);
-  }
-  const canonicalUrl = canonical.question({ first, second }, validatedNumber);
+  const currentQuestionNumber = questionNumberGuard(questionNumber);
+  const canonicalUrl = canonical.question({ first, second }, currentQuestionNumber);
   return generateCalculatorMetadata({ key: first, group: second, canonicalUrl });
 }
 
