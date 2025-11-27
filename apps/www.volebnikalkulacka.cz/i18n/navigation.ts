15eabcee3322bfa0b3c853eb9071b0848bf0d548
Kryštof Korb <krystof@korb.cz>
2025-11-27 13:16:07 +0100

    Remove unused navigation setup

--- apps/www.volebnikalkulacka.cz/i18n/navigation.ts
+++
@@ -1,5 +0,0 @@
-import { createNavigation } from "next-intl/navigation";
-
-import { routing } from "@/i18n/routing";
-
-export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
