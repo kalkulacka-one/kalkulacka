2d359501c11b466cd5b01ad04f3dc60db42b4ccc
Kryštof Korb <krystof@korb.cz>
2025-12-12 12:02:41 +0100

    Fix `vitest.config.ts`

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -1,11 +1,15 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": rootDir,
     },
   },
   test: {
