3492a90eb70c67f73ac6dc6f06e5d948f3a27174
Kryštof Korb <krystof@korb.cz>
2025-12-12 11:52:37 +0100

    Set up @ import alias in app package

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
