85747dc72a2e80d4bd262ea7f83f3aef92f376c0
Kryštof Korb <krystof@korb.cz>
2025-12-14 15:39:12 +0100

    Remove re-exports from Czech app barrels

--- apps/www.volebnikalkulacka.cz/calculator/client.ts
+++ apps/www.volebnikalkulacka.cz/calculator/client.ts
@@ -1,5 +1,4 @@
 "use client";
 
-export * from "@kalkulacka-one/app/client";
 export * from "./components/client";
 export * from "./view-models/client";
--- apps/www.volebnikalkulacka.cz/calculator/index.ts
+++ apps/www.volebnikalkulacka.cz/calculator/index.ts
@@ -1,2 +1,1 @@
-export * from "@kalkulacka-one/app";
 export * from "./components/server";
