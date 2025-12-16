3279418c66e5226d369ca0bd3e9eda45f22eddf9
mewdev <michal.wierzgon@gmail.com>
2025-12-16 10:16:41 +0100

    Refactor return statement

--- packages/app/src/data-fetching/calculator-data-loader.ts
+++ packages/app/src/data-fetching/calculator-data-loader.ts
@@ -3,8 +3,7 @@ import { NotFoundError } from "@/errors";
 
 export async function calculatorDataLoader({ endpoint, key, group, onNotFound }: { endpoint: string; key: string; group?: string; onNotFound: () => never }): Promise<CalculatorData> {
   try {
-    const calculatorData = await loadCalculatorData({ endpoint, key, group });
-    return calculatorData;
+    return await loadCalculatorData({ endpoint, key, group });
   } catch (error) {
     if (error instanceof NotFoundError) {
       onNotFound();
