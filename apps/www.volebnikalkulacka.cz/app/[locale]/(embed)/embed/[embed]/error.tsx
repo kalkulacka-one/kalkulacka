2f3332a9688c79ef93a5a1aa8a78e89af3023235
Michal Skop <michal.skop@kohovolit.eu>
2026-01-12 03:56:50 +0100

    lint fix

--- apps/www.volebnikalkulacka.cz/app/[locale]/(embed)/embed/[embed]/error.tsx
+++ apps/www.volebnikalkulacka.cz/app/[locale]/(embed)/embed/[embed]/error.tsx
@@ -2,6 +2,6 @@
 
 import { ErrorReporter } from "@/components/client";
 
-export default function ErrorBoundary({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+export default function ErrorBoundary({ error, reset: _reset }: { error: Error & { digest?: string }; reset: () => void }) {
   return <ErrorReporter error={error} />;
 }
