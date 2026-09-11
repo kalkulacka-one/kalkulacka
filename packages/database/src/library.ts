7dfc001cf7c83548be865a1ad4ee83451a44839d
Kryštof Korb <krystof@korb.cz>
2026-09-11 20:58:36 +0200

    Upgrade to Prisma 7 (#508)

--- packages/database/src/library.ts
+++ packages/database/src/library.ts
@@ -1,1 +1,4 @@
-export { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { Prisma } from "./generated/prisma/client.js";
+
+export const PrismaClientKnownRequestError: typeof Prisma.PrismaClientKnownRequestError = Prisma.PrismaClientKnownRequestError;
+export type PrismaClientKnownRequestError = Prisma.PrismaClientKnownRequestError;
